11a6d8ba1d378594c7ac80f7d4db6f6c7275e231
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2022-06-26 20:49:16 +0200

    intital project setup

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Hello from "./components/Hello.vue";
+</script>
+
+<template>
+  <head>
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+  </head>
+  <Hello></Hello>
+</template>
+
+
---
+++ src/components/Hello.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+    <h1 class="text-5xl font-bold text-blue-600">hot dev summer let's gooo</h1>
+</template>
