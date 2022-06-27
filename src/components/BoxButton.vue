b1c93f4d9973d3cf822301b1fd076643bd57f74a
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2022-06-27 12:45:22 +0200

    add: BoxButton component

---
+++ src/components/BoxButton.vue
@@ -0,0 +1,45 @@
+
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+    width: {
+        type: Number,
+        required: true
+    },
+    type: {
+        type: String,
+        default: 'secondary',
+        validator(value) {
+            return ['primary', 'secondary'].includes(value);
+        }
+    }
+})
+
+const classes = computed(() => {
+    return {
+        'text-white': props.type == 'primary',
+        'bg-lime-600' : props.type == 'primary',
+        'active:bg-orange-600' : props.type == 'primary',
+        'focus:border-orange-600' : props.type == 'primary',
+        
+        'text-green-600' : props.type == 'secondary',
+        'border-green-600' : props.type == 'secondary',
+        'active:text-orange-600' : props.type == 'secondary',
+        'active:border-orange-600' : props.type == 'secondary',
+        'focus:border-orange-600' : props.type == 'secondary',
+    }
+})
+
+</script>
+
+<template>
+    <button
+        :style="{ width: `${width}ch`}"
+        :class="classes"
+        class="box-content p-4 text-2xl font-bold text-left uppercase break-all border hover:lowercase focus:outline-none"
+    >
+        <slot></slot>
+    </button>
+</template>
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,18 +1,22 @@
 <template>
     <main class="flex flex-col space-y-20 ">
-        <h1 class="flex-grow text-8xl font-mono font-bold text-lime-600">orkide<br>.com</h1>
+        <h1 class="flex-grow font-mono font-bold text-8xl text-lime-600">orkide<br>.com</h1>
     
-        <div class="flex justify-between items-end">
-            <div class="flex items-end font-bold text-2xl text-orange-600 ">
+        <div class="flex items-end justify-between">
+            <div class="flex items-end text-2xl font-bold text-orange-600 ">
                 <p style="text-orientation: upright; writing-mode: vertical-rl;">MERCH&nbsp;</p>
                 <p style="text-orientation: upright; writing-mode: vertical-rl;">COMIN</p>
                 <p class="leading-none">02. <br> 05. <br> 22.</p>
             </div>
-            <div class="text-lime-600 flex gap-4 text-xl font-bold">
-                <button>SOCIALS</button>
-                <button>PROJECTS</button>
+            <div class="flex gap-4 text-xl font-bold text-lime-600">
+                <BoxButton :width="3">@socials*</BoxButton>
+                <BoxButton :width="3" type="primary">projects~</BoxButton> 
             </div>
         </div>
     </main>
 
 </template>
+
+<script setup>
+import BoxButton from './BoxButton.vue';
+</script>
