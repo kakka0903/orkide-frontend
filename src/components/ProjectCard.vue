2f11c398c7be1745b3e862c9042a79e71ca4f4c5
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2022-06-27 13:51:11 +0200

    add: project cards

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <article class="flex flex-col border-2 border-lime-600">
+        <img :src="imgurl" alt="">
+        <div class="flex gap-2 border-t-2 divide-x-2 divide-lime-600 border-lime-600 text-lime-600 divide-solid">
+            <p class="flex-grow px-4 py-2 ">
+                {{artist}}:<span class="text-orange-600">{{title}}</span> 
+                YR:<span class="text-orange-600 ">{{year}}</span>
+            </p>
+            <a :href="url" class="px-4 py-2 underline visited:text-lime-800 active:text-orange-600">WATCH</a>
+        </div>
+    </article>
+</template>
+
+<script setup>
+defineProps({
+    artist: {
+        type:String,
+        required: true,
+    },
+    title: {
+        type:String,
+        required: true,
+    },
+    year: {
+        type: Number,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    imgurl: {
+        type: String,
+        required: true,
+    }
+})
+</script>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,9 +1,13 @@
 <template>
-    <main class="font-bold space-y-5">
-        <h1 class="text-lime-900 text-5xl">PROJECTS</h1>
-        <h2 class="text-orange-600 text-xl">music vids for days</h2>
-        <p>project 1</p>
-        <p>project 2</p>
-        <p>project 3</p>
+    <main class="space-y-5 font-bold">
+        <h1 class="text-5xl text-lime-900">PROJECTS</h1>
+        <h2 class="text-xl text-orange-600">music vids for days</h2>
+        <ProjectCard artist="UG" title="TRAPMOBILE" :year="2022" url="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow/videos" imgurl="album.png"/>
+        <ProjectCard artist="UG" title="TRAPMOBILE" :year="2022" url="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow/videos" imgurl="album.png"/>
+        <ProjectCard artist="UG" title="TRAPMOBILE" :year="2022" url="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow/videos" imgurl="album.png"/>
     </main>
 </template>
+
+<script setup>
+import ProjectCard from './ProjectCard.vue';
+</script>
