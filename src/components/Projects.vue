866684de510b722da3e60cc7517135ce652c6a80
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2022-06-27 18:43:47 +0200

    add: spacing in projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,10 +1,12 @@
 <template>
-    <main class="font-bold">
+    <main class="space-y-4 font-bold">
         <h1 class="text-5xl text-primary-600">PROJECTS</h1>
         <h2 class="text-xl text-orange-600">music vids for days</h2>
-        <ProjectCard artist="UG" title="TRAPMOBILE" :year="2022" url="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow/videos" imgurl="album.png"/>
-        <ProjectCard artist="UG" title="TRAPMOBILE" :year="2022" url="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow/videos" imgurl="album.png"/>
-        <ProjectCard artist="UG" title="TRAPMOBILE" :year="2022" url="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow/videos" imgurl="album.png"/>
+        <section class="space-y-4">
+            <ProjectCard artist="UG" title="TRAPMOBILE" :year="2022" url="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow/videos" imgurl="album.png"/>
+            <ProjectCard artist="UG" title="TRAPMOBILE" :year="2022" url="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow/videos" imgurl="album.png"/>
+            <ProjectCard artist="UG" title="TRAPMOBILE" :year="2022" url="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow/videos" imgurl="album.png"/>
+        </section>
     </main>
 </template>
 
