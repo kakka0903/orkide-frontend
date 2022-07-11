b1f88b9a9bec39a9960c9f75886224ad388e4fbd
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-11 19:12:06 +0200

    add: PageHeading component

---
+++ src/components/PageHeading.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="relative text-primary-600 font-bold mt-10 mb-12">
+        <svg class="text-secondary-600 w-72 -translate-x-5 stroke-current" viewBox="0 0 735.255 133.583"><path data-name="Path 62" d="M735.193 66.52 81.458 79.33l-14.481 53.468L56.395 79.33.695 66.52l55.7-13.366L66.977.798l14.481 52.356Z" fill="none" stroke-width="6"/></svg>
+        <h1 class="absolute top-[-5px] left-[18px] text-[43px]">{{title}}</h1>
+        <h2 v-if="description">{{description}}</h2>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    title:Text,
+    description:Text,
+})
+</script>
