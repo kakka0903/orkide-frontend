8056cf43219e5d7088e589b54e5148004266c1b4
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-11 21:18:06 +0200

    fix: spacing bug footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 
 <template>
-    <footer class="flex justify-between my-4 text-lg text-secondary-dark ">
+    <footer class="flex justify-between py-4 text-lg text-secondary-dark ">
         <TimeZoneClock/>
         <p class="italic"> <a class="active:text-secondary" href="https://kakka.dev">kasper</a>  x lahnzki</p>
     </footer>
