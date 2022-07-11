aa33d7ba22a5cbbfb6089aa3422dd7e9d0e401a7
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-11 21:13:47 +0200

    refactor: footer tag in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 
 <template>
-    <div class="flex justify-between my-4 text-lg text-secondary-dark ">
+    <footer class="flex justify-between my-4 text-lg text-secondary-dark ">
         <TimeZoneClock/>
         <p class="italic"> <a class="active:text-secondary" href="https://kakka.dev">kasper</a>  x lahnzki</p>
-    </div>
+    </footer>
 </template>
 
 <script setup>
