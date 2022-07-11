6afc708fd0f4adaf20a644e07a2c278bc14950a2
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-11 21:29:39 +0200

    refactor: restyle and add links for socials page

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -1,8 +1,10 @@
 <template>
     <main class="flex flex-col">
         <PageHeading title="LAHN LIFE" description="Posts from the heart of lahnzki"/>
-        <a href="" class="relative pl-4 font-mono font-bold text-secondary text-7xl">INSTA</a>
-        <a href="" class="relative pl-4 font-mono font-bold text-secondary text-7xl">YOUTUBE</a>
+        <div class="flex flex-col items-center font-mono font-bold text-secondary text-8xl">
+            <a class="bg-white text-primary hover:bg-primary hover:text-white" href="https://www.instagram.com/orkide.co/">INSTA</a>
+            <a class="bg-white text-primary hover:bg-primary hover:text-white" href="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow">YTUBE</a>
+        </div>
     </main>
 </template>
 
