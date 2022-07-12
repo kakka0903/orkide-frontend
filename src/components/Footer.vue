1cde15466f37a35e97ada4479ad497f6f73256db
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-12 20:07:57 +0200

    add: underline to dev plug

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
 <template>
     <footer class="flex justify-between py-8 text-base text-secondary-dark ">
         <TimeZoneClock/>
-        <p class="italic"> <a class="active:text-secondary" href="https://kakka.dev">kasper</a>  x lahnzki</p>
+        <p class="italic"> <a class="underline active:text-bg-secondary" href="https://kakka.dev">kasper</a>  x lahnzki</p>
     </footer>
 </template>
 
