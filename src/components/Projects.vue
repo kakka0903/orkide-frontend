56d2d3324febdc4f81fa08e202a577668a31693b
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-12 20:05:12 +0200

    add: explainer text to projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,6 +1,7 @@
 <template>
     <main class="flex flex-col font-bold">
         <PageHeading title="LAHN CINEMA" description="Get a ticket and enjoy"/>
+        <p class="mb-10 text-primary">Music videos filmed, edited and directed by <span class="text-secondary">Magnus “Lahnzki” Lahn</span> under the <span class="text-secondary">orkide</span> name. </p>
         <div class="flex flex-col flex-grow mb-2 space-y-5">
             <ProjectTicket title="TRAPMOBILE" artist="UNDERGRUNN" length="2min 50s" date="SEP 2020"/>
             <ProjectTicket title="TIPP TOPP" artist="UNDERGRUNN" length="1min 20s" date="AUG 2022"/>
