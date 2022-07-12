c7161940cc829d16ec0f3622c45ec28db5e05eea
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-12 17:27:27 +0200

    refactor: change class ordering

--- src/components/PageHeading.vue
+++ src/components/PageHeading.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="relative text-primary font-bold mt-10 mb-12">
-        <svg class="text-secondary w-72 -translate-x-5 stroke-current" viewBox="0 0 735.255 133.583"><path data-name="Path 62" d="M735.193 66.52 81.458 79.33l-14.481 53.468L56.395 79.33.695 66.52l55.7-13.366L66.977.798l14.481 52.356Z" fill="none" stroke-width="6"/></svg>
+    <div class="relative mt-10 mb-12 font-bold text-primary">
+        <svg class="-translate-x-5 stroke-current text-secondary w-72" viewBox="0 0 735.255 133.583"><path data-name="Path 62" d="M735.193 66.52 81.458 79.33l-14.481 53.468L56.395 79.33.695 66.52l55.7-13.366L66.977.798l14.481 52.356Z" fill="none" stroke-width="6"/></svg>
         <h1 class="absolute top-[-5px] left-[18px] text-[43px]">{{title}}</h1>
         <h2 v-if="description">{{description}}</h2>
     </div>
