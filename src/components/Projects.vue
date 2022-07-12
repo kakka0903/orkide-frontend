181c345518a07aa0fe52fc4141a45da6c2cd2349
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-12 19:59:37 +0200

    fix: remove extra space

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,7 +1,7 @@
 <template>
     <main class="flex flex-col font-bold">
-        <div class="flex flex-col flex-grow mb-10 space-y-5">
         <PageHeading title="LAHN CINEMA" description="Get a ticket and enjoy"/>
+        <div class="flex flex-col flex-grow mb-2 space-y-5">
             <ProjectTicket title="TRAPMOBILE" artist="UNDERGRUNN" length="2min 50s" date="SEP 2020"/>
             <ProjectTicket title="TIPP TOPP" artist="UNDERGRUNN" length="1min 20s" date="AUG 2022"/>
             <ProjectTicket title="ITALIA" artist="UNDERGRUNN" length="2min 2s" date="AUG 2022"/>
