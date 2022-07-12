7c049f884c3c500685e984b5b4de55391b297439
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-13 01:58:20 +0200

    refactor: resize landing again

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,18 +1,18 @@
 <template>
     <main class="flex flex-col">
-        <h1 class="mt-12 font-bold text-7xl text-primary">orkide<br>.com</h1>
+        <h1 class="mt-2 leading-none font-bold text-[87.5px] text-primary">orkide<br>.com</h1>
 
         <div class="flex items-center justify-center flex-grow">
-            <SentencePoll  class="mb-20 text-lg" sentence="i really enjoy %o? " :options="['life', 'sex']" :intitalSelection="0" />
+            <SentencePoll  class="mb-20 text-2xl" sentence="i really enjoy %o? " :options="['life', 'sex']" :intitalSelection="0" />
         </div>
 
         <div class="flex items-end justify-between">
-            <div class="flex items-end text-xl font-bold text-secondary ">
+            <div class="flex items-end text-2xl font-bold text-secondary ">
                 <p style="text-orientation: upright; writing-mode: vertical-rl;">MERCH&nbsp;</p>
                 <p style="text-orientation: upright; writing-mode: vertical-rl;">COMIN</p>
                 <p class="leading-none">02. <br> 05. <br> 22.</p>
             </div>
-            <div class="flex items-end text-lg font-bold gap-x-4 text-primary">
+            <div class="flex items-end text-2xl font-bold gap-x-4 text-primary">
                 <BoxButton :width="3" @click="$emit('navigate', 'Socials')" >social</BoxButton>
                 <BoxButton :width="3" @click="$emit('navigate', 'Projects')" type="primary">projects~</BoxButton>
             </div>
