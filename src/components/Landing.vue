75c6901d15ae6efc116a51d898c4178eca002e31
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-13 13:01:50 +0200

    add: animations on landing page

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,10 +1,23 @@
 <template>
-    <main class="flex flex-col">
-        <h1 class="mt-2 font-bold leading-none text-7xl xs:text-8xl text-primary">orkide<br>.com</h1>
+    <main class="flex flex-col tra">
 
-        <div class="flex items-center justify-center flex-grow">
-            <SentencePoll  class="mb-20 text-2xl" sentence="i really enjoy %o? " :options="['life', 'sex']" :intitalSelection="0" />
-        </div>
+        <transition appear
+            enter-active-class="transition duration-500 ease-out transform-gpu"
+            enter-from-class="translate-x-10 opacity-0"
+            enter-to-class="scale-100 translate-x-0 opacity-100"
+        >
+        <h1 class="mt-2 font-bold leading-none text-7xl xs:text-8xl text-primary">orkide<br>.com</h1>
+        </transition>
+
+        <transition appear
+            enter-active-class="transition duration-500 ease-out delay-700 transform-gpu"
+            enter-from-class="translate-x-10 opacity-0"
+            enter-to-class="scale-100 translate-x-0 opacity-100"
+        >
+            <div class="flex items-center justify-center flex-grow delay-150">
+                <SentencePoll  class="mb-20 text-2xl" sentence="i really enjoy %o? " :options="['life', 'sex']" :intitalSelection="0" />
+            </div>
+        </transition>
 
         <div class="flex items-end justify-between">
             <div class="flex items-end text-2xl font-bold text-secondary ">
@@ -12,10 +25,16 @@
                 <p style="text-orientation: upright; writing-mode: vertical-rl;">COMIN</p>
                 <p class="leading-none">02. <br> 05. <br> 22.</p>
             </div>
+            <transition appear
+                enter-active-class="transition duration-500 ease-out delay-300 transform-gpu"
+                enter-from-class="translate-x-10 opacity-0"
+                enter-to-class="scale-100 translate-x-0 opacity-100"
+            >
             <div class="flex items-end text-2xl font-bold gap-x-4 text-primary">
                 <BoxButton :width="3" @click="$emit('navigate', 'Socials')" >social</BoxButton>
                 <BoxButton :width="3" @click="$emit('navigate', 'Projects')" type="primary">projects~</BoxButton>
             </div>
+            </transition>
         </div>
     </main>
 </template>
