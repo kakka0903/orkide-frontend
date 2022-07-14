fc05cecf5c473ee7981ae49c03721bf544c581a5
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-14 19:48:38 +0200

    add: built in background to videomodal

--- src/components/VideoModal.vue
+++ src/components/VideoModal.vue
@@ -1,6 +1,7 @@
 <template>
+    <button @click="$emit('close')" class="fixed inset-0 top-0 left-0 z-10 w-full h-full bg-black opacity-50 "></button>
     <div class="absolute top-0 left-0 flex items-center justify-center w-full h-full px-10">
-        <article class="relative max-w-sm font-bold border-4 border-secondary text-primary">
+        <article class="relative z-50 max-w-sm font-bold border-4 border-secondary text-primary">
             <button @click="$emit('close')" class="absolute text-3xl top-2 right-4 text-primary hover:text-primary-dark active:text-secondary">X</button>
             <iframe class="w-full" height="225" :src="embedlink" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <div class="p-4 bg-white border-t-4 border-secondary">
