62819c4cf3c1777e372c62ea9a5d6460ef8416dc
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-14 13:47:55 +0200

    add: active style to socials

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -2,8 +2,8 @@
     <main class="flex flex-col">
         <PageHeading title="LAHN LIFE" description="Posts from the heart of lahnzki"/>
         <div class="flex flex-col items-center font-mono font-bold text-secondary text-8xl">
-            <a class="bg-white text-primary hover:bg-primary hover:text-white" href="https://www.instagram.com/orkide.co/">INSTA</a>
-            <a class="bg-white text-primary hover:bg-primary hover:text-white" href="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow">YTUBE</a>
+            <a class="bg-white text-primary hover:bg-primary active:text-secondary hover:text-white" href="https://www.instagram.com/orkide.co/">INSTA</a>
+            <a class="bg-white text-primary hover:bg-primary active:text-secondary hover:text-white" href="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow">YTUBE</a>
         </div>
     </main>
 </template>
