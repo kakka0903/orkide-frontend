cf3b917ee1d82d86621b44ab218e8b03b121207b
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-15 02:15:59 +0200

    add: description for trap mobile

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -5,7 +5,7 @@
         <div class="flex flex-col flex-grow mb-2 space-y-2">
             <ProjectTicket @click="isVideoModalOpen = false" title="TIPP TOPP" description=". This project was codenamed 'generic song 3' since information about Orkidé projects kept leaking to the masses." artist="UNDERGRUNN" length="1min 20s" date="AUG 2022"/>
             <ProjectTicket @click="isVideoModalOpen = false" title="ITALIA" description=" is the third music video produced by Orkidé. During a week in Italy the whole video was shot, edited and published. " artist="UNDERGRUNN" length="2min 2s" date="AUG 2022"/>
-            <ProjectTicket @click="isVideoModalOpen = false" title="TRAPMOBILE" description=". dsadas" artist="UNDERGRUNN" length="2min 50s" date="SEP 2020"/>
+            <ProjectTicket @click="isVideoModalOpen = false" title="TRAPMOBILE" description=". This music video is actually banned in saudi arabia since it features the use of so called narcotics." artist="UNDERGRUNN" length="2min 50s" date="SEP 2020"/>
         </div>
         <VideoModal @close="isVideoModalOpen = !isVideoModalOpen" v-if="isVideoModalOpen" title="TRAPMOBILE" year="2020" description="Some description about the video. Most ambitious project by the orkide team ever created. Directed and edited by @williamnymoen and @lahnzki" artist="UNDERGRUNN" embedlink="https://www.youtube.com/embed/Ri3k4XB2e7c"/>
     </main>
