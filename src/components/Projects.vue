0f4f8d181fb05df7544a9c2ee34c197820daabe9
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-17 01:22:24 +0200

    style: prettify projects html

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,7 +1,14 @@
 <template>
     <main class="flex flex-col font-bold">
-        <PageHeading title="LAHN CINEMA" description="Get a ticket and enjoy"/>
-        <p class="mb-10 text-primary">Music videos filmed, edited and directed by <span class="text-secondary">Magnus “Lahnzki” Lahn</span> under the <span class="text-secondary">orkide</span> name. </p>
+        <PageHeading
+            title="LAHN CINEMA"
+            description="Get a ticket and enjoy"
+        />
+        <p class="mb-10 text-primary">
+            Music videos filmed, edited and directed by
+            <span class="text-secondary">Magnus “Lahnzki” Lahn</span>
+            under the <span class="text-secondary">orkide</span> name.
+        </p>
         <div class="flex flex-col flex-grow mb-2 space-y-2">
             <ProjectTicket
                 v-for="musicVideo in musicVideos"
