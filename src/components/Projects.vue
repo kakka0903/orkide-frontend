6af785bb75e907c93f2b33b892b417e1b18f7180
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-17 01:20:08 +0200

    refactor: use musicVideo object

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,9 +3,11 @@
         <PageHeading title="LAHN CINEMA" description="Get a ticket and enjoy"/>
         <p class="mb-10 text-primary">Music videos filmed, edited and directed by <span class="text-secondary">Magnus “Lahnzki” Lahn</span> under the <span class="text-secondary">orkide</span> name. </p>
         <div class="flex flex-col flex-grow mb-2 space-y-2">
-            <ProjectTicket @click="isVideoModalOpen = false" title="TIPP TOPP" description=". This project was codenamed 'generic song 3' since information about Orkidé projects kept leaking to the masses." embedLink="" artist="UNDERGRUNN" length="1min 20s" date="AUG 2022"/>
-            <ProjectTicket @click="isVideoModalOpen = false" title="ITALIA" description=" is the third music video produced by Orkidé. During a week in Italy the whole video was shot, edited and published. " embedLink="" artist="UNDERGRUNN" length="2min 2s" date="AUG 2022"/>
-            <ProjectTicket @click="isVideoModalOpen = false" title="TRAPMOBILE" description=". This music video is actually banned in saudi arabia since it features the use of so called narcotics." embedLink="https://www.youtube.com/embed/Ri3k4XB2e7c" artist="UNDERGRUNN" length="2min 50s" date="SEP 2020"/>
+            <ProjectTicket
+                v-for="musicVideo in musicVideos"
+                :musicVideo="musicVideo"
+                :key="musicVideo.id"
+            />
         </div>
     </main>
 </template>
@@ -14,4 +16,6 @@
 import PageHeading from './PageHeading.vue';
 import ProjectCard from './ProjectCard.vue';
 import ProjectTicket from './ProjectTicket.vue';
+import useMusicVideos from '../composables/useMusicVideos.js';
+const { musicVideos } = useMusicVideos();
 </script>
--- src/components/VideoModal.vue
+++ src/components/VideoModal.vue
@@ -3,8 +3,8 @@
         <iframe class="w-full bg-black text-primary" height="225" :src="embedlink" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>no video</iframe>
         <div class="flex items-center p-4 bg-white border-t-4 border-secondary">
             <div class="">
-                <h3 class="text-3xl leading-none text-secondary">{{title}}</h3>
-                <p class="text-sm ">{{artist}} - {{year}}</p>
+                <h3 class="text-3xl leading-none uppercase text-secondary">{{musicVideo.name}}</h3>
+                <p class="text-sm uppercase">{{musicVideo.artist}} - {{musicVideo.year}}</p>
             </div>
         </div>
     </BoxModal>
@@ -13,9 +13,9 @@
 <script setup>
 import BoxModal from './BoxModal.vue';
 defineProps({
-    embedlink:String,
-    title:String,
-    artist:String,
-    year:Number,
+    musicVideo:{
+        type:Object,
+        required:true,
+    }
 })
 </script>
