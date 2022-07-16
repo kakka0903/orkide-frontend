f48c3eedc2b1f4c7fb8e886f62663ceda903e3a9
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-16 20:13:48 +0200

    add: transition to BoxModal

--- src/components/BoxModal.vue
+++ src/components/BoxModal.vue
@@ -1,14 +1,26 @@
 <template>
-    <div v-if="isOpen" @click.stop="close" class="fixed inset-0 top-0 left-0 z-50 flex items-center justify-center text-left bg-black bg-opacity-50 cursor-default">
-        <article @click.stop class="relative bg-white border-4 border-secondary">
-            <button @click="close" class="absolute lowercase top-2 right-2 text-primary hover:text-primary-dark active:text-secondary-dark">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-            </button>
-            <slot></slot>
-        </article>
-    </div>
+    <transition appear
+        enter-active-class="transition duration-150"
+        enter-from-class="opacity-0"
+        enter-to-class="opacity-100"
+    >
+        <div v-if="isOpen" @click.stop="close" class="fixed inset-0 top-0 left-0 z-50 flex items-center justify-center text-left bg-black bg-opacity-50 cursor-default">
+            <transition appear
+                enter-active-class="transition duration-500 delay-150 transform"
+                enter-from-class="-translate-y-6 opacity-0"
+                enter-to-class="translate-y-0 opacity-100"
+            >
+                <article @click.stop class="relative bg-white border-4 border-secondary">
+                    <button @click="close" class="absolute lowercase top-2 right-2 text-primary hover:text-primary-dark active:text-secondary-dark">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                    <slot></slot>
+                </article>
+            </transition>
+        </div>
+    </transition>
 </template>
 
 <script setup>
