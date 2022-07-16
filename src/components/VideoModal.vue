8a11063ee971ad20a530ae6759dd6b7fa3091d98
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-16 19:33:24 +0200

    refactor: rewrite videomodal uses BoxModal

--- src/components/VideoModal.vue
+++ src/components/VideoModal.vue
@@ -1,21 +1,16 @@
 <template>
-    <div @click="$emit('close')" class="fixed inset-0 top-0 left-0 z-50 flex items-center justify-center bg-black bg-opacity-50 pointer">
-        <article class="relative max-w-sm font-bold border-4 border-secondary text-primary">
-            <button @click="$emit('close')" class="absolute text-3xl top-2 right-4 text-primary hover:text-primary-dark active:text-secondary">X</button>
-            <iframe class="w-full" height="225" :src="embedlink" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <div class="p-4 bg-white border-t-4 border-secondary">
-                <h3 class="text-3xl leading-none text-secondary">{{title}}</h3>
-                <p class="mb-4 text-sm ">{{artist}} - {{year}}</p>
-                <p class="text-sm font-normal">{{description}}</p>
-            </div>
-        </article>
-    </div>
+    <BoxModal :isOpen="true" class="text-primary-dark">
+        <iframe class="w-full bg-black text-primary" height="225" :src="embedlink" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>no video</iframe>
+        <div class="p-4 bg-white border-t-4 border-secondary">
+            <h3 class="text-3xl leading-none text-secondary">{{title}}</h3>
+            <p class="mb-4 text-sm ">{{artist}} - {{year}}</p>
+            <p class="text-sm font-normal">{{description}}</p>
+        </div>
+    </BoxModal>
 </template>
 
 <script setup>
-
-
-
+import BoxModal from './BoxModal.vue';
 defineProps({
     embedlink:String,
     title:String,
