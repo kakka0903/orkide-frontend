73f4bdc4bfd4daeb00b56cd7008e035012bf6bf2
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-18 01:19:25 +0200

    add: blink smiley component

---
+++ src/components/BlinkSmiley.vue
@@ -0,0 +1,14 @@
+<template>
+    <span>{{text}}</span>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+var text = ref(':)');
+setTimeout(() => {
+    text.value = ';)'
+    setTimeout(() => {
+        text.value = ':)'
+    }, 300);
+}, 1000);
+</script>
