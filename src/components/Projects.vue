1b6e360cfcdd329733d5ea48332fab55fd35716c
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-18 01:41:28 +0200

    add: hardcode musicVideos

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -20,9 +20,13 @@
 </template>
 
 <script setup>
+import { ref } from '@vue/reactivity';
 import PageHeading from './PageHeading.vue';
 import ProjectCard from './ProjectCard.vue';
 import ProjectTicket from './ProjectTicket.vue';
-import useMusicVideos from '../composables/useMusicVideos.js';
-const { musicVideos } = useMusicVideos();
+const musicVideos = ref([
+    { id:1, youtubeId:"Ri3k4XB2e7c", name: 'Trapmobile', description: 'is the third music video produced by Orkidé. The trapmobile seen in the videos, is a Volvo station wagon.', artist: 'Undergrunn', length: 165, year: 2020},
+    { id:2, youtubeId:"2NR29EicJP4", name: 'DR.PLAZA', description: 'This music video is actually banned in saudi arabia since it features the use of narcotics', artist: 'Undergrunn', length: 238, year: 2019},
+    { id:3, youtubeId:"KaCIVZKTjv4", name: 'Hjemmebrent', description: 'This project was codenamed "generic song 3" since information orkidé projects kept leaking to the masses.', artist: 'MONSTER', length: 199, year: 2019},
+])
 </script>
