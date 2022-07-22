d68da302df039e16d2663d141d50a9f4fe20ba18
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-22 17:03:49 +0200

    add: tiktok link

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -3,7 +3,8 @@
         <PageHeading title="LAHN LIFE" description="Posts from the heart of lahnzki"/>
         <div class="flex flex-col items-center font-mono font-bold text-secondary text-8xl">
             <a class="bg-white text-primary hover:bg-primary active:text-secondary hover:text-white" href="https://www.instagram.com/orkide.co/">INSTA</a>
-            <a class="bg-white text-primary hover:bg-primary active:text-secondary hover:text-white" href="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow">YTUBE</a>
+            <a class="bg-white text-secondary hover:bg-secondary active:text-primary hover:text-white" href="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow">YOUTUBE</a>
+            <a class="bg-white text-primary hover:bg-primary active:text-secondary hover:text-white" href="https://www.tiktok.com/@orkide.co">TIKTOK</a>
         </div>
     </main>
 </template>
