56183630e35a924cd1508327e94fc0bc59dddf6f
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-22 17:16:30 +0200

    fix: socials text on small screens

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -1,7 +1,7 @@
 <template>
     <main class="flex flex-col">
         <PageHeading title="LAHN LIFE" description="Posts from the heart of lahnzki"/>
-        <div class="flex flex-col items-center font-mono font-bold text-secondary text-8xl">
+        <div class="flex flex-col items-center font-mono font-bold text-secondary text-7xl sm:text-8xl">
             <a class="bg-white text-primary hover:bg-primary active:text-secondary hover:text-white" href="https://www.instagram.com/orkide.co/">INSTA</a>
             <a class="bg-white text-secondary hover:bg-secondary active:text-primary hover:text-white" href="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow">YOUTUBE</a>
             <a class="bg-white text-primary hover:bg-primary active:text-secondary hover:text-white" href="https://www.tiktok.com/@orkide.co">TIKTOK</a>
