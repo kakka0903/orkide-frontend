4f1d52abfa1c45964cfbce40a275e6cc0b10f572
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-07-22 16:15:56 +0200

    refactor: use heroicons library

--- src/components/BoxAlert.vue
+++ src/components/BoxAlert.vue
@@ -16,12 +16,8 @@
                 :class="{ 'border-secondary' : level == 'warning', 'border-primary': level == 'success'}"
             >
                 <div class="ml-3 mr-2 mt-0.5">
-                    <svg v-if="level=='warning'" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-                    </svg>
-                    <svg v-if="level=='success'" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 stroke-current" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
+                    <ExclamationIcon v-if="level=='warning'" class="w-6 h-6"/>
+                    <CheckCircleIcon v-if="level=='success'" class="w-6 h-6"/>
                 </div>
                 <div class="mx-2">
                     <p class="text-lg uppercase ">{{title}}</p>
@@ -33,6 +29,7 @@
 </template>
 
 <script setup>
+import { ExclamationIcon, CheckCircleIcon } from '@heroicons/vue/outline'
 import { onMounted, ref } from "@vue/runtime-core";
 
 defineProps({
@@ -40,7 +37,8 @@ defineProps({
         type:String,
         validator(value) {
             return ['warning', 'success'].includes(value);
-        }
+        },
+        default: "warning",
     },
     message:String,
     title:String,
--- src/components/BoxModal.vue
+++ src/components/BoxModal.vue
@@ -12,9 +12,7 @@
             >
                 <article @click.stop class="relative bg-white border-4 border-secondary">
                     <button @click="close" class="absolute lowercase top-2 right-2 text-primary hover:text-primary-dark active:text-secondary-dark">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
+                        <XIcon class="w-6 h-6"/>
                     </button>
                     <slot></slot>
                 </article>
@@ -24,6 +22,8 @@
 </template>
 
 <script setup>
+import { XIcon } from '@heroicons/vue/outline'
+
 const emit = defineEmits(['close'])
 defineProps({
     isOpen:Boolean
