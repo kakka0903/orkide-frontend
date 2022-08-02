701ba5a025eee567014c63353c092723c50adf5b
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-08-02 13:52:40 +0200

    add: BTSCard

---
+++ src/components/BTSCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <article
+        @click="isModalOpen = true"
+        :style="backgroundStyle"
+        class="flex items-end justify-end h-32 p-2 bg-white border-4 cursor-pointer hover:border-secondary hover:text-secondary text-primary sm:col-span-1 border-primary"
+    >
+        <p class="">#{{id}}</p>
+        <BoxModal :isOpen="isModalOpen" @close="isModalOpen = false">
+            <div class="w-full max-w-sm">
+                <YoutubeEmbed :youtubeId="youtubeId" />
+            </div>
+        </BoxModal>
+    </article>
+</template>
+
+<script setup>
+
+import { computed, ref } from 'vue'
+import BoxModal from './BoxModal.vue';
+import YoutubeEmbed from './YoutubeEmbed.vue';
+const isModalOpen = ref(false);
+
+
+const props = defineProps({
+    id: Number,
+    imageUrl: String,
+    youtubeId: String,
+})
+
+const backgroundStyle = computed(() => {
+    return {
+        "background-image" : "url( " + props.imageUrl + " )",
+        'background-size': 'cover',
+    }
+})
+</script>
+
