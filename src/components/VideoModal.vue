9a9c08a8e9afa54ff6f73a27039b87d15f4129fb
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-08-02 13:52:24 +0200

    add: youtubeEmbed component

--- src/components/VideoModal.vue
+++ src/components/VideoModal.vue
@@ -1,6 +1,6 @@
 <template>
     <BoxModal :isOpen="true" class="text-primary">
-        <iframe class="w-full bg-black text-primary" height="225" :src="embedlink" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <YoutubeEmbed :youtubeId="musicVideo.youtubeId"/>
         <div class="flex items-center p-4 bg-white border-t-4 border-secondary">
             <div class="">
                 <h3 class="text-3xl leading-none uppercase text-secondary">{{musicVideo.name}}</h3>
@@ -15,13 +15,11 @@
 import { computed } from '@vue/runtime-core';
 import BoxModal from './BoxModal.vue';
 import ReactionPoll from './ReactionPoll.vue';
+import YoutubeEmbed from './YoutubeEmbed.vue';
 const props = defineProps({
     musicVideo:{
         type:Object,
         required:true,
     }
-})
-const embedlink = computed( () => {
-    return 'https://www.youtube.com/embed/'+props.musicVideo.youtubeId;
 })
 </script>
---
+++ src/components/YoutubeEmbed.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="">
+        <iframe
+            class="w-full bg-black text-primary"
+            :height="playerHeight"
+            :src="embedLink"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+        ></iframe>
+    </div>
+</template>
+
+<script setup>
+import { computed } from "@vue/runtime-core"
+
+const props = defineProps({
+    playerHeight: {
+        type: Number,
+        default: 225,
+    },
+    youtubeId: String,
+})
+
+const embedLink = computed(() => {
+    return 'https://www.youtube.com/embed/'+props.youtubeId;
+})
+</script>
