e6ad3af17dbcbc88f1d6cb063d5114afc99d8a1b
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-08-03 13:51:08 +0200

    refactor: projects in NO

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,13 +1,15 @@
 <template>
     <main class="flex flex-col font-bold">
         <PageHeading
-            title="LAHN CINEMA"
-            description="Get a ticket and enjoy"
+            title="ORKIDÉ KINO"
+            description="Musikkvideoer laget av Orkidé"
         />
         <p class="mb-10 text-primary">
-            Music videos filmed, edited and directed by
-            <span class="text-secondary">Magnus “Lahnzki” Lahn</span>
-            under the <span class="text-secondary">orkide</span> name.
+            EKSTRA! SE eksklusive
+            <span class="italic text-secondary">behind the scenes</span>
+            klipp fra filmingen av Orkidé musikkvideoer. Trykk
+            <span class="italic">BTS</span>
+            nedenfor <blink-smiley/>
         </p>
         <div class="flex flex-col content-start flex-grow gap-2 mb-2 sm:gap-y-3 sm:justify-center sm:items-start sm:flex-wrap sm:flex-row">
             <ProjectTicket
@@ -24,6 +26,7 @@ import { ref } from '@vue/reactivity';
 import PageHeading from './PageHeading.vue';
 import ProjectCard from './ProjectCard.vue';
 import ProjectTicket from './ProjectTicket.vue';
+import BlinkSmiley from './BlinkSmiley.vue'
 const musicVideos = ref([
     { id:1, youtubeId:"Ri3k4XB2e7c", name: 'Trapmobile', description: 'is the third music video produced by Orkidé. The trapmobile seen in the videos, is a Volvo station wagon.', artist: 'Undergrunn', length: 165, year: 2020},
     { id:2, youtubeId:"2NR29EicJP4", name: 'DR.PLAZA', description: 'This music video is actually banned in saudi arabia since it features the use of narcotics', artist: 'Undergrunn', length: 238, year: 2019},
