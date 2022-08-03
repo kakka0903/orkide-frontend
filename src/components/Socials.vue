db2e833e78deaf1b2dd1d8411970d9ee21788182
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-08-03 13:59:45 +0200

    refactor: socials in NO

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -1,6 +1,6 @@
 <template>
     <main class="flex flex-col">
-        <PageHeading title="LAHN LIFE" description="Posts from the heart of lahnzki"/>
+        <PageHeading title="ORKIDÉ LIFE" description="Oppdater deg på alt Orkidé & UG"/>
         <div class="flex flex-col items-center font-mono font-bold text-secondary text-7xl sm:text-8xl">
             <a class="bg-white text-primary hover:bg-primary active:text-secondary hover:text-white" href="https://www.instagram.com/orkide.co/">INSTA</a>
             <a class="bg-white text-secondary hover:bg-secondary active:text-primary hover:text-white" href="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow">YOUTUBE</a>
