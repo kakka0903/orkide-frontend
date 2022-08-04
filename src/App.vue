516c090e5888b742a4426b00bfcb48fffbf32a94
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-08-04 16:41:42 +0200

    add: bts section

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,16 @@ import Landing from "./components/Landing.vue";
 import Projects from "./components/Projects.vue";
 import Footer from "./components/Footer.vue";
 import Socials from "./components/Socials.vue";
+import BehindTheScenes from './components/BehindTheScenes.vue';
 import { ref } from 'vue';
 
+function viewBTS(musicVideo) {
+  selectedMusicVideo.value = musicVideo;
+  currentPage.value = 'bts';
+}
 const pages = ref(['orkidé', 'kontakt', 'prosjekter'])
 const currentPage = ref(pages.value[0])
+const selectedMusicVideo = ref(NaN);
 </script>
 
 <template>
@@ -20,8 +26,9 @@ const currentPage = ref(pages.value[0])
   <div style="height: 100vh;" class="flex flex-col mx-6 font-mono xs:mx-10">
     <Navigation :pages="pages" :currentPage="currentPage" @navigate="(page) => currentPage = page"/>
     <Landing class="flex-grow" @navigate="(page) => currentPage = page" v-if="currentPage == 'orkidé'"/>
-    <Projects class="flex-grow" v-if="currentPage == 'prosjekter'"/>
+    <Projects class="flex-grow" @bts="(musicVideo) => viewBTS(musicVideo)" v-if="currentPage == 'prosjekter'"/>
     <Socials class="flex-grow" v-if="currentPage == 'kontakt'"/>
+    <BehindTheScenes class="flex-grow" v-if="currentPage == 'bts'" :musicVideo="selectedMusicVideo"/>
     <Footer></Footer>
   </div>
 </template>
