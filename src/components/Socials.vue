77c32e7e48e6c3f6ba5ad910a7d8609005aa620e
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-08-05 23:43:26 +0200

    add: more spacing in socials

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -1,12 +1,12 @@
 <template>
     <main class="flex flex-col">
         <PageHeading title="ORKIDÉ LIFE" description="Oppdater deg på alt Orkidé & UG"/>
-        <div class="flex flex-col items-center font-mono font-bold text-secondary text-7xl sm:text-8xl">
+        <div class="flex flex-col items-center mt-12 mb-24 font-mono font-bold text-secondary text-7xl sm:text-8xl">
             <a class="bg-white text-primary hover:bg-primary active:text-secondary hover:text-white" href="https://www.instagram.com/orkide.co/">INSTA</a>
             <a class="bg-white text-secondary hover:bg-secondary active:text-primary hover:text-white" href="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow">YOUTUBE</a>
             <a class="bg-white text-primary hover:bg-primary active:text-secondary hover:text-white" href="https://www.tiktok.com/@orkide.co">TIKTOK</a>
         </div>
-        <p class="my-12 font-bold text-primary">For mer seriøse henveldelser skyt meg en epost på <a class="underline text-secondary" href="mailto:lahnzki@gmail.com">lahnzki@gmail.com</a>, så tar jeg kontakt når det passer. </p>
+        <p class="mb-12 font-bold text-primary">For mer seriøse henveldelser skyt meg en epost på <a class="underline text-secondary" href="mailto:lahnzki@gmail.com">lahnzki@gmail.com</a>, så tar jeg kontakt når det passer. </p>
     </main>
 </template>
 
