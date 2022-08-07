4eb5740197edc1eab58cedde03e44ec8feda4699
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-08-07 11:57:34 +0200

    change footer text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
 <template>
     <footer class="flex justify-between py-6 text-base xs:py-8 text-secondary-dark ">
         <TimeZoneClock/>
-        <p class="italic"> <a class="underline active:text-bg-secondary" href="https://kakka.dev">kasper</a>  x lahnzki</p>
+        <p class="italic">developed by <a class="underline active:text-bg-secondary" href="https://kakka.dev">kasper</a></p>
     </footer>
 </template>
 
