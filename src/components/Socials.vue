b6ae06325676169e5c7280fa060e592ff350dd45
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-08-11 18:06:54 +0200

    add: insta fix

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -2,7 +2,7 @@
     <main class="flex flex-col">
         <PageHeading title="ORKIDÉ LIFE" description="Oppdater deg på alt Orkidé & UG"/>
         <div class="flex flex-col items-center mt-12 mb-24 font-mono font-bold text-secondary text-7xl sm:text-8xl">
-            <a class="bg-white text-primary hover:bg-primary active:text-secondary hover:text-white" href="https://www.instagram.com/orkide.co/">INSTA</a>
+            <a class="bg-white text-primary hover:bg-primary active:text-secondary hover:text-white" href="https://www.instagram.com/orkide.world/">INSTA</a>
             <a class="bg-white text-secondary hover:bg-secondary active:text-primary hover:text-white" href="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow">YOUTUBE</a>
             <a class="bg-white text-primary hover:bg-primary active:text-secondary hover:text-white" href="https://www.tiktok.com/@orkide.co">TIKTOK</a>
         </div>
