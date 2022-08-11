1fa8910795ef69d1aea49c795bd2b24ea75fee6c
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-08-11 18:20:02 +0200

    fix: tiktok url :((

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -4,7 +4,7 @@
         <div class="flex flex-col items-center mt-12 mb-24 font-mono font-bold text-secondary text-7xl sm:text-8xl">
             <a class="bg-white text-primary hover:bg-primary active:text-secondary hover:text-white" href="https://www.instagram.com/orkide.world/">INSTA</a>
             <a class="bg-white text-secondary hover:bg-secondary active:text-primary hover:text-white" href="https://www.youtube.com/channel/UCWc-1iefSoth8CuMxzNFxow">YOUTUBE</a>
-            <a class="bg-white text-primary hover:bg-primary active:text-secondary hover:text-white" href="https://www.tiktok.com/@orkide.co">TIKTOK</a>
+            <a class="bg-white text-primary hover:bg-primary active:text-secondary hover:text-white" href="https://www.tiktok.com/@orkide.world">TIKTOK</a>
         </div>
         <p class="font-bold text-primary">For business ta kontakt på <a class="underline text-secondary" href="mailto:magnuslahn@gmail.com">magnuslahn@gmail.com</a></p>
     </main>
