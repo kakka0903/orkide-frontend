733cd8b1183b58359f423fb3b2f6a4319b868c90
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-09-04 17:21:15 +0200

    fix: colors of alert

--- components/BoxAlert.vue
+++ components/BoxAlert.vue
@@ -7,20 +7,20 @@
     >
         <div
             v-if="!isDeleted"
-            :class="{ 'text-secondary' : level=='warning', 'text-primary' : level=='success'}"
+            :class="{ 'text-red-600' : level=='warning', 'text-primary' : level=='success'}"
             class="fixed top-0 left-0 flex justify-center w-full cursor-pointer pointer-events-none"
         >
             <article
-                @click="isDeleted = true"
-                class="flex w-full max-w-sm py-3 mt-5 -mx-2 bg-white border-4 pointer-events-auto "
-                :class="{ 'border-secondary' : level == 'warning', 'border-primary': level == 'success'}"
+                @click.stop="isDeleted = true"
+                class="flex w-full max-w-sm py-3 mt-5 -mx-2 bg-white border-4 pointer-events-auto"
+                :class="{ 'border-red-600' : level == 'warning', 'border-primary': level == 'success'}"
             >
                 <div class="ml-3 mr-2 mt-0.5">
                     <ExclamationIcon v-if="level=='warning'" class="w-6 h-6"/>
                     <CheckCircleIcon v-if="level=='success'" class="w-6 h-6"/>
                 </div>
                 <div class="mx-2">
-                    <p class="text-lg uppercase ">{{title}}</p>
+                    <p class="text-lg text-left uppercase">{{title}}</p>
                     <p class="text-sm">{{message}}</p>
                 </div>
             </article>
