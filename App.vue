43471b7697e7212dd02515f9ae8e77eb38bc1fcb
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-09-05 22:17:18 +0200

    add: default head

--- App.vue
+++ App.vue
@@ -1,11 +1,22 @@
 <template>
-  <head>
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
-  </head>
   <div class="flex flex-col min-h-screen mx-6 font-mono xs:mx-10">
     <Navigation :paths="['/kontakt', '/prosjekter']"/>
     <NuxtPage/>
     <Footer></Footer>
   </div>
 </template>
+<script setup>
+useHead({
+  htmlAttrs: {
+    lang: 'no'
+  },
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
+  ],
+  script:[
+    { defer:'defer', src:'https://plausible.kakk.dev/js/plausible.js', 'data-domain':'orkide.world'}
+  ],
+})
+
+</script>
 
