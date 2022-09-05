31f0fef348c16625b9f849a9e48dad8ae9529c8c
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-09-05 21:30:37 +0200

    refactor: poll transition within

--- components/PollSequence.vue
+++ components/PollSequence.vue
@@ -1,15 +1,24 @@
 <template>
-    <div v-if="showPoll">
-        <SentencePoll
-            v-if="pollsIndex < polls.length"
-            :sentence="currentPoll.sentence"
-            :options="currentPoll.options"
-            @selected="progress()"
-        />
-        <p class="text-primary" v-else>
-            thanks
-            <BlinkSmiley/>
-        </p>
+    <div>
+        <Transition
+            enter-active-class="transition duration-150 ease-out transform-gpu"
+            enter-from-class="translate-x-10 opacity-0"
+            leave-active-class="transition duration-150 ease-in transform-gpu"
+            leave-to-class="-translate-x-10 opacity-0"
+        >
+            <div v-if="showPoll">
+                <SentencePoll
+                    v-if="pollsIndex < polls.length"
+                    :sentence="currentPoll.sentence"
+                    :options="currentPoll.options"
+                    @selected="progress()"
+                />
+                <p class="text-primary" v-else>
+                    thanks
+                    <BlinkSmiley/>
+                </p>
+            </div>
+        </Transition>
     </div>
 </template>
 
