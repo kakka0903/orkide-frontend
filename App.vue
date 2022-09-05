5f2d381c84e617ef1a5b814d65b2cb8128fb7bb6
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-09-05 19:21:11 +0200

    add: current page indicator

--- App.vue
+++ App.vue
@@ -3,7 +3,7 @@
     <link rel="icon" type="image/x-icon" href="favicon.ico">
   </head>
   <div style="height: 100vh;" class="flex flex-col mx-6 font-mono xs:mx-10">
-    <Navigation/>
+    <Navigation :paths="['/kontakt', '/prosjekter']"/>
     <NuxtPage/>
     <Footer></Footer>
   </div>
--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,9 +1,27 @@
 
 <template>
     <nav class="flex items-center justify-between gap-6 py-6 text-base font-bold xs:py-8 text-primary-dark">
-        <NuxtLink  to="/" class="mr-auto lowercase active:text-secondary-dark hover:uppercase">órkide</NuxtLink>
-        <NuxtLink  to="/kontakt" class="lowercase active:text-secondary-dark hover:uppercase">kontakt</NuxtLink>
-        <NuxtLink  to="/prosjekter" class="lowercase active:text-secondary-dark hover:uppercase">prosjekter</NuxtLink>
+        <NuxtLink  to="/" :class="{ 'text-secondary' : route.path == '/'}" class="mr-auto lowercase active:text-secondary-dark hover:uppercase">órkide</NuxtLink>
+        <NuxtLink
+            :class="{ 'text-secondary' : route.path == path}"
+            class="lowercase first:mr-auto active:text-secondary-dark hover:uppercase"
+            v-for="path in paths"
+            :key="path"
+            :to="path"
+        >
+            {{path.substring(1)}}
+        </NuxtLink>
     </nav>
 </template>
 
+<script setup>
+import { onMounted } from 'vue';
+
+const route = useRoute();
+
+defineProps({
+    paths: {
+        required: true,
+    }
+})
+</script>
