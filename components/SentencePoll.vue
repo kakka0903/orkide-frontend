ca36a3b278739a3bfaef709072893a71f2ca9662
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-09-07 01:02:07 +0200

    refactor: sentencepoll form

--- components/SentencePoll.vue
+++ components/SentencePoll.vue
@@ -1,51 +1,86 @@
 <script setup>
-    import { watch } from 'vue'
-
+    const emit = defineEmits(['selected'])
     const props = defineProps({
         sentence: String,
         options: Array,
     })
 
-    defineEmits(['selected'])
-    var selected = ref(props.options[0])
+    const formElement = ref();
+
+    var selectedOption = ref(props.options[0])
+    var hasChosen = ref(false)
+    function selectOption(option) {
+        selectedOption.value = option;
+        hasChosen.value = true;
+        emit('selected', selectedOption);
+        // formElement.value.submit()
+    }
 
     var selectedIndex = computed(() => {
         for(var i = 0; i < props.options.length; i++ ) {
-            if (selected.value == props.options[i]) {
+            if (selectedOption.value == props.options[i]) {
                 return i;
             }
         }
         return -1;
     })
 
-    /* reset chosen and selected when props change */
-    watch(props, () => {
-        chosen.value = false;
-        selected.value = props.options[0];
+    /* styles to align selected option with sentence */
+    const translateStyles = computed(() => {
+        let percent = 100/(props.options.length)*selectedIndex.value;
+        return { transform: 'translateY(-'+percent+'%)' }
     })
 
     const optionIndex = computed(() => props.sentence.indexOf("%o"))
     const preOption = computed(() => props.sentence.substring(0, props.sentence.indexOf("%o")))
     const postOption = computed(() => props.sentence.substring(props.sentence.indexOf("%o") + 2, props.sentence.length))
-    const translate = computed(() => selectedIndex*10)
-    var chosen = ref(false)
 </script>
 
 <template>
-<div class="text-primary">
-    <span class="lowercase">
-    {{preOption}}
-    <div :class="{ 'border-primary' : !chosen, 'border-secondary': chosen}" class="relative inline-block border-b-4 border-dotted group">
-        <span class="opacity-0">{{selected}}</span>
-        <div class="absolute top-0 left-0 flex flex-col transition-all duration-150 transform-gpu" :style="{transform: 'translateY(-'+(100/(options.length)*selectedIndex)+'%)'}">
-            <button class="mb-2 text-left hover:uppercase disabled:pointer-events-none whitespace-nowrap" :disabled="chosen"
-                  :class="{'text-secondary' : option == selected && chosen, 'opacity-25': chosen && option != selected}"
-                  :key="option" v-for="option in options"
-                  @click="selected = option; chosen = true; $emit('selected', selected)">{{option}}</button>
-        </div>
-    </div>
-    {{postOption}}
-    </span>
-</div>
+    <form
+        :name="sentence"
+        ref="formElement"
+        method="post"
+    >
+        <span class="lowercase text-primary">
+
+            {{preOption}}
+
+            <div
+                :class="{ 'border-primary' : !hasChosen, 'border-secondary': hasChosen}"
+                class="relative inline-block border-b-4 border-dotted"
+            >
+                <span class="opacity-0">{{selectedOption}}</span>
+                <div
+                    class="absolute top-0 left-0 flex flex-col transition-all duration-150 transform-gpu"
+                    :style="translateStyles"
+                >
+                    <div
+                        class="mb-2 text-left"
+                        :name="sentence"
+                        :key="option" v-for="option in options"
+                        @click="selectOption(option)"
+                    >
+                        <input
+                            class="hidden"
+                            :value="option"
+                            :disabled="hasChosen"
+                            name="option"
+                            type="radio"
+                        >
+                        <label
+                            class="cursor-pointer hover:uppercase whitespace-nowrap"
+                            :class="{'text-secondary' : option == selectedOption && hasChosen, 'opacity-25': hasChosen && option != selectedOption}"
+                            :for="option"
+                            v-text="option"
+                        />
+                    </div>
+                </div>
+            </div>
+
+            {{postOption}}
+
+        </span>
+    </form>
 </template>
 
