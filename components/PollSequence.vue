66751797fa36221f5a34f33eb25befe8395ce5cb
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-09-07 01:00:00 +0200

    refactor: no hiding polls

--- components/PollSequence.vue
+++ components/PollSequence.vue
@@ -1,24 +1,26 @@
 <template>
     <div>
-        <Transition
+        <TransitionGroup
             enter-active-class="transition duration-150 ease-out transform-gpu"
             enter-from-class="translate-x-10 opacity-0"
             leave-active-class="transition duration-150 ease-in transform-gpu"
             leave-to-class="-translate-x-10 opacity-0"
         >
-            <div v-if="showPoll">
-                <SentencePoll
-                    v-if="pollsIndex < polls.length"
-                    :sentence="currentPoll.sentence"
-                    :options="currentPoll.options"
-                    @selected="progress()"
-                />
-                <p class="text-primary" v-else>
-                    thanks
-                    <BlinkSmiley/>
-                </p>
-            </div>
-        </Transition>
+            <SentencePoll
+                v-for="poll in polls"
+                :key="polls.indexOf(poll)"
+                v-show="pollsIndex == polls.indexOf(poll) && showPoll"
+                :sentence="poll.sentence"
+                :options="poll.options"
+                @selected="progress()"
+            />
+            <p
+                v-if="pollsIndex == polls.length && showPoll"
+                class="text-primary"
+            >
+                thanks <BlinkSmiley/>
+            </p>
+        </TransitionGroup>
     </div>
 </template>
 
