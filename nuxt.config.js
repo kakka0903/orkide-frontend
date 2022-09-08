053d5203b493d5cb661e6a4c63fc1572d5ffded1
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-09-08 17:08:14 +0200

    add: auto route generation

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,17 @@
 import { defineNuxtConfig } from 'nuxt'
 
+import projects from './data/projects';
+const dynamicRoutes = []
+projects.forEach((project) => {
+    dynamicRoutes.push('/bts/'+project.name);
+})
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     target: 'static',
     mode: 'universal',
-
-    buildModules: ['@nuxtjs/tailwindcss']
+    buildModules: ['@nuxtjs/tailwindcss'],
+    generate: {
+        routes: dynamicRoutes
+    }
 })
