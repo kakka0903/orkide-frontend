e367ac719ed482388e3478980b19fcb7d2027658
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-09-08 15:33:18 +0200

    refactor: extract projects data

---
+++ data/projects.js
@@ -0,0 +1,73 @@
+const projects = [
+    {
+        id:0,
+        youtubeId:"VCTC2wkUgUE",
+        name: 'ITALIA',
+        description: 'Pakk bagen, åpne vin flaska og bli med til Italia',
+        artist: 'Undergrunn',
+        length: 188,
+        year: 2022,
+        isNew: true,
+        clips: [
+            {
+                number:1,
+                description: "hello!"
+            },
+            {
+                number:2,
+                description: "hello!"
+            },
+            {
+                number:3,
+                description: "hello!"
+            },
+            {
+                number:4,
+                description: "hello!"
+            },
+        ]
+    },
+    {
+        id:1,
+        youtubeId:"1meKrSyHErM",
+        name: 'TOPPEN AV OSL',
+        description: 'Fin vin, fin bil. Bli med til toppen. Det er sånn vi har det her.',
+        artist: 'Undergrunn',
+        length: 190,
+        year: 2021,
+        clips: []
+    },
+    {
+        id:2,
+        youtubeId:"55cqJAV-Lhs",
+        name: 'KOM OG BETAL',
+        description: 'Alexander McQueen sko jeg leser en fin bok, bak i en lynet McQueen coupe.',
+        artist: 'Undergrunn',
+        length: 176,
+        year: 2020,
+        clips: []
+    },
+    {
+        id:3,
+        youtubeId:"Ri3k4XB2e7c",
+        name: 'TRAPMOBILE',
+        description: 'er den tredje musikkvideoen produsert av Orkidé for UG. Bli med inn i en toyota hiace selvom mamma sier nei.',
+        artist: 'Undergrunn',
+        length: 165,
+        year: 2020,
+
+        clips: []
+    },
+    {
+        id:4,
+        youtubeId:"2NR29EicJP4",
+        name: 'DR.PLAZA',
+        description: 'Her started sammarbeidet som ble bølgen. Tsunami.',
+        artist: 'Undergrunn',
+        length: 238,
+        year: 2019,
+        clips: []
+    },
+];
+
+export default projects;
