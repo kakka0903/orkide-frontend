d866a77737495127c671c17909921beedebef3bf
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2022-09-11 15:49:10 +0200

    add: loading section

---
+++ components/LoadingSection.vue
@@ -0,0 +1,18 @@
+<template>
+    <transition
+        enter-active-class="transition-opacity duration-150"
+        enter-from-class="opacity-0"
+        enter-to-class="opacity-100"
+        leave-active-class="transition-opacity duration-150"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
+        mode="out-in"
+    >
+        <LoadingIndicator v-if="isLoading"/>
+        <slot v-else/>
+    </transition>
+</template>
+
+<script setup>
+defineProps({isLoading:Boolean})
+</script>
