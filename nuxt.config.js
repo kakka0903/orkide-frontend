d8889805a875584095ad95d381da64de56dcb5fc
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2022-09-11 17:48:03 +0200

    delete projects data file

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,19 +1,10 @@
 import { defineNuxtConfig } from 'nuxt'
 
-import projects from './data/projects';
-const dynamicRoutes = []
-projects.forEach((project) => {
-    dynamicRoutes.push('/bts/'+project.id);
-})
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     target: 'static',
     mode: 'universal',
     buildModules: ['@nuxtjs/tailwindcss', '@nuxtjs/strapi'],
-    generate: {
-        routes: dynamicRoutes
-    },
     strapi: {
         url: process.env.STRAPI_URL || 'http://localhost:1337'
     },
