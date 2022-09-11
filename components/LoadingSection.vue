3e257424d3753545fd1a5467797473a344e8c207
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2022-09-11 16:56:20 +0200

    fix: loading section layout jank

--- components/LoadingSection.vue
+++ components/LoadingSection.vue
@@ -1,18 +1,34 @@
 <template>
-    <transition
-        enter-active-class="transition-opacity duration-150"
-        enter-from-class="opacity-0"
-        enter-to-class="opacity-100"
-        leave-active-class="transition-opacity duration-150"
-        leave-from-class="opacity-100"
-        leave-to-class="opacity-0"
-        mode="out-in"
-    >
-        <LoadingIndicator v-if="isLoading"/>
-        <slot v-else/>
-    </transition>
+    <div>
+        <transition
+            enter-active-class="transition-opacity"
+            enter-from-class="opacity-0"
+            enter-to-class="opacity-100"
+            leave-active-class="transition-opacity"
+            leave-to-class="opacity-0"
+            leave-from-class="opacity-100"
+        >
+            <LoadingIndicator class="absolute" v-if="showIndicator"/>
+        </transition>
+        <transition
+            enter-active-class="transition-opacity"
+            enter-from-class="opacity-0"
+            enter-to-class="opacity-100"
+        >
+            <div v-show="!isLoading && !showIndicator">
+                <slot />
+            </div>
+        </transition>
+    </div>
 </template>
 
 <script setup>
-defineProps({isLoading:Boolean})
+const props = defineProps({isLoading:Boolean})
+
+const isWaitDone = ref(false);
+setTimeout(() => {
+    isWaitDone.value = true;
+}, 1000);
+
+const showIndicator = computed(() => { return props.isLoading && isWaitDone.value})
 </script>
