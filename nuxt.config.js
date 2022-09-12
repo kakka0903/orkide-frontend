c745c2ba5a2fef3aa542f2bdd8aaca3f23b42cd1
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-09-12 09:46:35 +0200

    add: generate dynamic routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 import { defineNuxtConfig } from 'nuxt'
+import axios from 'axios'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
@@ -8,4 +9,19 @@ export default defineNuxtConfig({
     strapi: {
         url: process.env.STRAPI_URL || 'http://localhost:1337'
     },
+    hooks: {
+      async 'nitro:config' (nitroConfig) {
+        if (nitroConfig.dev) { return }
+        const res = await axios.get(process.env.STRAPI_URL+'/api/projects?populate[0]=bts_clips');
+        res.data.data.forEach((project) => {
+            const slug = project.attributes.slug;
+            nitroConfig.prerender.routes.push('/prosjekter/'+slug)
+            nitroConfig.prerender.routes.push('/bts/'+slug)
+            const clips = project.attributes.bts_clips.data;
+            clips.forEach((clip) => {
+                nitroConfig.prerender.routes.push('/bts/'+slug+'/'+clips.indexOf(clip))
+            })
+        })
+      }
+    }
 })
