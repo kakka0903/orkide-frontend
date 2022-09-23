bb59babfe1eee4415b25234358578c128877fcdb
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-09-23 08:27:51 +0200

    add: load projects from cache

--- pages/prosjekter.vue
+++ pages/prosjekter.vue
@@ -4,42 +4,23 @@
             title="ORKIDÉ KINO"
             description="Videoer laget av Orkidé"
         />
-        <NuxtPage :projects="projects"/>
-        <LoadingSection :isLoading="isLoading">
-            <div class="flex flex-col content-start flex-grow gap-2 mb-2 gap-y-3 sm:flex-wrap sm:flex-row">
-                <ProjectTicket
-                    v-for="musicVideo in projects"
-                    @watch="watchVideo(musicVideo)"
-                    :musicVideo="musicVideo"
-                    :key="musicVideo.id"
-                />
-            </div>
-        </LoadingSection>
+        <NuxtPage :projects="projects.data"/>
+        <div class="flex flex-col content-start flex-grow gap-2 mb-2 gap-y-3 sm:flex-wrap sm:flex-row">
+            <ProjectTicket
+                v-for="musicVideo in projects.data"
+                @watch="watchVideo(musicVideo)"
+                :musicVideo="musicVideo"
+                :key="musicVideo.id"
+            />
+        </div>
     </main>
 </template>
 
-<script async setup>
+<script setup>
+import projects from '/data/projects.json'
+
 function watchVideo(musicVideo) {
     navigateTo('/prosjekter/'+musicVideo.attributes.slug+'/')
 }
-
-useHead({
-    title: 'Orkidé - Prosjekter',
-});
-
-const projects = ref([]);
-const { find } = useStrapi4();
-const isLoading = ref(true);
-
-try {
-    const res = await find('projects', {
-        populate: 'bts_clips',
-        sort: 'year:desc'
-    });
-    projects.value = res.data;
-    isLoading.value = false;
-} catch (e) {
-    console.log("error loading projects from CMS:")
-    console.log(e.error)
-}
+useHead({title: 'Orkidé - Prosjekter'});
 </script>
