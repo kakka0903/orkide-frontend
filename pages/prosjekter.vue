e50fb49f61afcbebfd9f22dcd51440cf592e68bc
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2023-06-06 21:02:48 +0200

    perf: add youtube preconnect

--- pages/prosjekter.vue
+++ pages/prosjekter.vue
@@ -19,6 +19,9 @@
 import projects from '/data/projects.json'
 useHead({
     title: 'Orkidé - Prosjekter',
-    meta:[{name:'description', content:'Se de nye Undergrunn videoene på Orkidé Kino. Alle videoer er produsert, filmet og redigert av Orkidé. Sjekk ut hits som ITALIA, DR.PLAZA og TRAPMOBILE.'}]
+    meta:[{name:'description', content:'Se de nye Undergrunn videoene på Orkidé Kino. Alle videoer er produsert, filmet og redigert av Orkidé. Sjekk ut hits som ITALIA, DR.PLAZA og TRAPMOBILE.'}],
+    link: [
+        { rel: 'preconnect', href: 'https://www.youtube.com'},
+    ],
 });
 </script>
