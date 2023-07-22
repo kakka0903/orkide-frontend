6b881ef611139ea1b09674b281d5ede44e8a79e3
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2023-07-22 18:36:07 +0200

    add: archive display

---
+++ components/archive/display.vue
@@ -0,0 +1,35 @@
+
+<template>
+    <div class="relative h-44 w-[340px]">
+        <ArchiveBox class="absolute bottom-0 z-[2]">ORKIDÉ ARCHIVE</ArchiveBox>
+        <div class="absolute z-[1] flex pl-1 bottom-3 transition">
+            <ArchiveFolderCover
+                v-for="folder in folders"
+                :isSelected="selectedId == folder.id && navigateFolders"
+                :isNavigating="navigateFolders"
+                :isAfterSelected="selectedIndex < folders.indexOf(folder)"
+            >
+                {{ folder.name }}
+            </ArchiveFolderCover>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    folders: Array,
+    selectedIndex: Number,
+    navigateFolders: Boolean
+})
+
+let selectedId = computed(() => {
+    return props.folders[props.selectedIndex].id;
+})
+
+const emit = defineEmits(['openFolder']);
+let openFolder = (folderId) => {
+    if(selectedId.value == folderId) {
+        emit('openFolder', folderId);
+    }
+}
+</script>
