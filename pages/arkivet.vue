a1d7a72e5df2ebe5e7850cb7d2016ebf6b537b66
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2023-07-23 00:58:03 +0200

    add: archive page

---
+++ pages/arkivet.vue
@@ -0,0 +1,49 @@
+<template>
+    <main class="flex flex-col flex-grow">
+        <PageHeading title="ARKIVET" description="Behind the scenes and cover art"></PageHeading>
+
+        <section class="flex flex-col items-center justify-center flex-grow w-full gap-5 pb-20">
+            <ArchiveDisplay
+                :folders="folders"
+                :selectedIndex="selectedIndex"
+                :isNavigating="isNavigating"
+                @clickBox="nextFolder(); hasInteracted = true"
+            />
+            <div class="h-10">
+                <Transition
+                    enter-active-class="transition duration-1000"
+                    enter-from-class="opacity-0"
+                >
+                    <ArchiveControls
+                        v-if="hasInteracted"
+                        v-model:selectedIndex="selectedIndex"
+                        v-model:isNavigating="isNavigating"
+                        :folderCount="folders.length"
+                    />
+                </Transition>
+            </div>
+        </section>
+    </main>
+</template>
+
+<script setup>
+let archiveFolders = ref([
+    { id: 4, name: 'mata:come'},
+    { id: 5, name: 'italia'},
+    { id: 6, name: 'egoland'},
+    { id: 7, name: 'UNDERGRUNN'},
+    { id: 8, name: '1'},
+]);
+
+const MAX_FOLDERS = 11;
+let folders = computed(() => {
+    return archiveFolders.value.slice(0, MAX_FOLDERS);
+});
+
+let selectedIndex = ref(0);
+let isNavigating = ref(false);
+let hasInteracted = ref(false);
+
+let { nextFolder } = useArchiveControls(selectedIndex, isNavigating, folders.value.length);
+</script>
+
