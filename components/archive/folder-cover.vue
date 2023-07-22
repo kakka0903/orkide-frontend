cb24f73abe9fd877e23fa50e23ecfbe986a6937d
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2023-07-22 18:56:40 +0200

    fix: missing slot

--- components/archive/folder-cover.vue
+++ components/archive/folder-cover.vue
@@ -3,7 +3,7 @@
         class="flex justify-center w-20 px-3 -mr-16 text-left break-all transition-all ease-out bg-white border-2 border-blue-600 h-28 text-primary-dark"
         :class="moveStyles"
     >
-        {{ folder.name }}
+        <slot/>
     </article>
 </template>
 
