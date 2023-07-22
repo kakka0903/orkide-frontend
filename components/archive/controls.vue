0d192bc9552cd53b89baecc8e350098c7c47969c
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2023-07-23 00:54:24 +0200

    refactor: redesign controls

--- components/archive/controls.vue
+++ components/archive/controls.vue
@@ -1,8 +1,20 @@
 <template>
-    <div class="flex gap-5">
-        <BoxButton @click="prevFolder" :width="2" type="secondary">PREV</BoxButton>
-        <BoxButton @click="openFolder" :width="3" type="primary" :disabled="!isNavigating">BROWSE</BoxButton>
-        <BoxButton @click="nextFolder" :width="2" type="secondary">NEXT</BoxButton>
+    <div class="flex items-center gap-5 text-primary-dark">
+        <button @click="prevFolder" class="transition disabled:opacity-0 hover:text-primary">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+            </svg>
+        </button>
+
+        <button @click="openFolder" :disabled="!isNavigating" class="transition disabled:opacity-50 hover:text-primary">
+            open
+        </button>
+
+        <button @click="nextFolder" class="transition disabled:opacity-0 hover:text-primary">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+            </svg>
+        </button>
     </div>
 </template>
 
