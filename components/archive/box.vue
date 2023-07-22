70a72ee7b1bdbf1167672cddd7503b1797b1b3c8
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2023-07-22 23:46:18 +0200

    refactor: green box

--- components/archive/box.vue
+++ components/archive/box.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="flex">
         <div>
-            <div class="w-64 h-4 bg-white border-[3px] border-b-0 border-primary"></div>
-            <div class="flex items-center justify-center w-64 h-20 text-sm bg-white border-[3px] border-primary text-primary">
+            <div class="w-64 h-4 bg-white border-[3px] border-b-0 border-secondary"></div>
+            <div class="flex items-center justify-center w-64 h-20 text-sm bg-white border-[3px] border-secondary text-secondary-dark">
                 <slot/>
             </div>
         </div>
         <div>
-            <div class="w-20 h-4 bg-white border-[3px] border-b-0 border-l-0 border-primary"></div>
-            <div class="w-20 h-20 text-sm bg-white border-[3px] border-l-0 border-primary"></div>
+            <div class="w-20 h-4 bg-white border-[3px] border-b-0 border-l-0 border-secondary"></div>
+            <div class="w-20 h-20 text-sm bg-white border-[3px] border-l-0 border-secondary"></div>
         </div>
     </div>
 </template>
