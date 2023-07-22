d99100562a840f99172ba40a1899ef92ab984997
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2023-07-22 18:32:20 +0200

    add: archive box

---
+++ components/archive/box.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex">
+        <div>
+            <div class="w-64 h-4 bg-white border-2 border-b-0 border-blue-600"></div>
+            <div class="flex items-center justify-center w-64 h-20 text-sm text-blue-600 bg-white border-2 border-blue-600">
+                <slot/>
+            </div>
+        </div>
+        <div>
+            <div class="w-20 h-4 bg-white border-2 border-b-0 border-l-0 border-blue-600"></div>
+            <div class="w-20 h-20 text-sm bg-white border-2 border-l-0 border-blue-600"></div>
+        </div>
+    </div>
+</template>
