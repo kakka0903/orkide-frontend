c16b737f77fe5fc181a8ebca193dd8e09eda6f97
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2023-08-24 18:56:27 +0200

    refactor: simplify folder icon

--- components/FolderIcon.vue
+++ components/FolderIcon.vue
@@ -1,15 +1,6 @@
 <template>
     <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 83 63">
-        <mask id="a" fill="#fff">
-            <path d="M0 0h37v25H0V0Z"/>
-        </mask>
-        <path d="M0 0h37v25H0V0Z" fill="#fff"/>
-        <path d="M0 0v-3h-3v3h3Zm37 0h3v-3h-3v3ZM0 3h37v-6H0v6Zm34-3v25h6V0h-6ZM3 25V0h-6v25h6Z" fill="#01F" mask="url(#a)"/>
-        <mask id="b" fill="#fff">
-            <path d="M34 6h49v41H34V6Z"/>
-        </mask>
-        <path d="M34 6h49v41H34V6Z" fill="#fff"/>
-        <path d="M83 6h3V3h-3v3Zm0 41v3h3v-3h-3ZM34 9h49V3H34v6Zm46-3v41h6V6h-6Zm3 38H34v6h49v-6Z" fill="#01F" mask="url(#b)"/>
-        <path fill="#fff" stroke="#01F" stroke-width="3" d="M1.5 14.5h80v47h-80z"/>
+        <path d="M37 7.5h44.5v54h-80v-60h34v6H37Z" class="stroke-current" fill="#fff" stroke="#01F" stroke-width="3"/>
+        <path d="M82 15H2" stroke="#01F" class="stroke-current" stroke-width="3"/>
     </svg>
 </template>
