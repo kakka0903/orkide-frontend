bb835854f88758b2009279061ceb007ac0f56297
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2023-08-24 13:11:58 +0200

    add: basic cover projects UI

--- pages/covers.vue
+++ pages/covers.vue
@@ -1,6 +1,23 @@
 <template>
     <main>
         <page-heading title="ORKIDÉ COVERS" description="Album covers made by Orkidé"></page-heading>
+        <div class="space-y-5">
+            <div
+                v-for="coverProject in coverProjects" :key="coverProject"
+                class="flex items-center gap-5"
+            >
+                <div class="w-10 h-10 border border-primary"></div>
+                <p>{{ coverProject.title }}</p>
+            </div>
+        </div>
     </main>
 </template>
 
+
+<script setup>
+const coverProjects = ref([
+    { id: 0, title: 'mata:come', slideshow: [1,2,3] },
+    { id: 1, title: 'undergrunn', slideshow: [1,2,3] },
+    { id: 2, title: 'kom og betal', slideshow: [1,2,3] }
+]);
+</script>
