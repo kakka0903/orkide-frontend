ab352955bb109d4a2638191b1cc88fd5406c2fea
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2023-08-24 15:21:35 +0200

    add: slideshows to covers page

--- pages/covers.vue
+++ pages/covers.vue
@@ -2,12 +2,15 @@
     <main>
         <page-heading title="ORKIDÉ COVERS" description="Album covers made by Orkidé"></page-heading>
         <div class="space-y-5">
-            <div
-                v-for="coverProject in coverProjects" :key="coverProject"
-                class="flex items-center gap-5"
-            >
-                <div class="w-10 h-10 border border-primary"></div>
-                <p>{{ coverProject.title }}</p>
+            <div v-for="coverProject in coverProjects" :key="coverProject" class="space-y-5">
+                <button class="flex items-center gap-5" @click="selectProject(coverProject.id)">
+                    <div class="w-10 h-10 border border-primary"></div>
+                    <p>{{ coverProject.title }}</p>
+                </button>
+                <ProjectSlideshow
+                    v-if="selectedProjectId == coverProject.id"
+                    :slides="slides"
+                />
             </div>
         </div>
     </main>
@@ -15,9 +18,24 @@
 
 
 <script setup>
+// TODO: add folder graphics
+// TODO: folder selected styling
+
 const coverProjects = ref([
-    { id: 0, title: 'mata:come', slideshow: [1,2,3] },
-    { id: 1, title: 'undergrunn', slideshow: [1,2,3] },
-    { id: 2, title: 'kom og betal', slideshow: [1,2,3] }
+    { id: 0, title: 'mata:come' },
+    { id: 1, title: 'undergrunn' },
+    { id: 2, title: 'kom og betal' }
 ]);
+
+const slides = ref([
+    { id: 0, image: '/italia-wrapped.png', description: 'Kommentar om dette bildet. Nevner at det er endelig versjon.'},
+    { id: 1, image: '/album.png', description: 'Paper Sketch #01'},
+    { id: 2, image: '/album.png', description: 'Paper Sketch #02'},
+])
+
+const selectedProjectId = ref(null);
+const selectProject = (projectId) => {
+    let currentVal = selectedProjectId.value
+    selectedProjectId.value = currentVal != projectId ? projectId  : null
+}
 </script>
