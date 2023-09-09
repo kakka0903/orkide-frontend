3eed9fb5b43c986d01e4597a40f73b597e0fbbb5
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2023-09-09 13:01:49 +0200

    refactor: better navigation

--- components/AlbumSlidesModal.vue
+++ components/AlbumSlidesModal.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="fixed inset-0 flex items-center justify-center pt-5 bg-black/70">
-        <carousel :items-to-show="1">
+    <div class="fixed inset-0 flex items-center justify-center pt-5 bg-black/70" @click="exit">
+        <Carousel ref="slideshow" :items-to-show="1" class="relative">
             <slide v-for="slide in slides" :key="slide.id" class="flex justify-center h-full">
-                <div class="w-full max-w-lg p-10 space-y-20">
+                <div class="w-full max-w-lg p-10 space-y-20" @click.stop>
                     <img class="w-full bg-gray-500 shadow-2xl aspect-square" :src="getImageAttrs(slide).url">
                     <!-- <img :src="slide.image" style="object-fit: cover"> -->
                     <div class="px-10 py-5 mx-5 font-bold text-left bg-white shadow-2xl niceBorder text-primary">
@@ -10,11 +10,21 @@
                     </div>
                 </div>
             </slide>
-        </carousel>
+        </Carousel>
 
-        <BoxButton @click="navigateTo('/covers')" type="primary" class="fixed top-5 right-5" :width="2">
-            BACK
-        </BoxButton>
+        <arrow-controls
+            class="absolute bottom-7"
+            @click.stop
+            v-if="slideshow"
+            @next="slideshow.next"
+            @prev="slideshow.prev"
+            :current="currentSlide"
+            :count="slideshow.data.slidesCount.value"
+        />
+
+        <button @click="exit" class="absolute lowercase top-10 right-10 text-secondary-dark hover:text-secondary active:text-secondary">
+            <XIcon class="w-6 h-6"/>
+        </button>
     </div>
 </template>
 
@@ -30,12 +40,22 @@
 // TODO: add title slide
 import 'vue3-carousel/dist/carousel.css'
 import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+import { XIcon } from '@heroicons/vue/solid/index.js';
 
-const props = defineProps({
+defineProps({
     slides: Array
+})
+
+const slideshow = ref(null);
+const currentSlide = computed(() => {
+    return slideshow.value.data.currentSlide.value + 1
 })
 
 const getImageAttrs = (slide) => {
     return slide.image.data.attributes
+}
+
+const exit = () => {
+    navigateTo('/covers')
 }
 </script>
