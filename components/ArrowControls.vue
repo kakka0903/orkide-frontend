975ec51469870bf1f96517abf60776b619bdcba6
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2023-09-09 13:01:29 +0200

    add: arrow controls

---
+++ components/ArrowControls.vue
@@ -0,0 +1,38 @@
+
+<template>
+    <div class="flex items-center gap-10 text-secondary-dark">
+        <button @click="prev" class="transition disabled:opacity-0 hover:text-secondary" :disabled="current == 1">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+            </svg>
+        </button>
+
+        <div>
+            {{ current }} / {{ count }}
+        </div>
+
+        <button @click="next" class="transition disabled:opacity-0 hover:text-secondary" :disabled="current == count">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+            </svg>
+        </button>
+    </div>
+</template>
+
+
+<script setup>
+defineProps({
+    current: Number,
+    count: Number
+})
+
+const emit = defineEmits([
+    'next', 'prev'
+])
+const next = () => {
+    emit('next')
+}
+const prev = () => {
+    emit('prev')
+}
+</script>
