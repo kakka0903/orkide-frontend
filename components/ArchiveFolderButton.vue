1d277401a9c9fd025c8dc4fe8a7dfca2c93d52b3
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2023-10-15 18:44:53 +0200

    add: folder button component

---
+++ components/ArchiveFolderButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="flex items-center gap-5 group">
+    <div class="border-b-2 border-dashed w-7 border-primary" />
+    <FolderIcon class="w-16 h-16 transition text-primary group-hover:text-secondary-dark group-active:text-secondary group-visited:text-primary-dark" />
+    <p class="text-lg font-bold underline transition text-primary group-hover:text-secondary-dark group-active:text-secondary group-visited:text-primary-dark">
+      <slot />
+    </p>
+  </button>
+</template>
