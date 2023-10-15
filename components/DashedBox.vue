047efab87a5da586e86ed7d408c6a6a07aec7951
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2023-10-15 17:41:37 +0200

    add: page introduction

---
+++ components/DashedBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="p-7 dashed-border">
+    <p class="text-2xl font-bold text-primary">{{ title }}</p>
+    <p v-if="subtitle" class="font-bold uppercase text-secondary">{{ subtitle }}</p>
+    <p class="mt-3 text-primary">{{ text }}</p>
+    <slot ></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title: string
+  subtitle?: string
+  text: string
+}
+defineProps<Props>()
+</script>
--- pages/covers.vue
+++ pages/covers.vue
@@ -1,6 +1,14 @@
 <template>
     <main class="flex-grow">
-        <page-heading title="COVERART" description="Discover albumart made by Orkidé"></page-heading>
+        <page-heading :title="PAGE_TITLE" :description="PAGE_SUBTITLE"></page-heading>
+        <div class="pb-20">
+            <DashedBox
+                class="max-w-md"
+                :title="INTRO_TITLE"
+                :subtitle="INTRO_SUBTITLE"
+                :text="INTRO_TEXT"
+            />
+        </div>
         <div class="flex flex-row items-stretch">
             <div class="border-r-2 border-dashed border-primary"></div>
             <div class="space-y-5">
@@ -18,7 +26,15 @@
 </template>
 
 
-<script setup>
+<script setup lang="ts">
+
+const PAGE_TITLE = 'COVERART'
+const PAGE_SUBTITLE = 'Discover albumart made by Orkidé'
+
+const INTRO_TITLE = 'Lytt og Utforsk'
+const INTRO_SUBTITLE = 'UTFORSK PROSESSEN BAK ORKIDÉ ALBUMCOVERE'
+const INTRO_TEXT = 'I dette arkivet av coverart prosjekter kan du utforske orkide sin prosjekt-utviklings prosess gjennom skisser, utkast og tankekart. Bruk Spotify til å lytte til musikken mens du utforsker for den beste opplevelsen.'
+
 // TODO: add folder graphics
 // TODO: folder selected styling
 
