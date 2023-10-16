63c4238401cc52cb22543b24853ebc166d55d956
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2023-10-17 01:02:14 +0200

    add: description transition

--- components/Covers/ImageSlide.vue
+++ components/Covers/ImageSlide.vue
@@ -1,8 +1,18 @@
 <template>
   <CoversBaseSlide>
-    <img v-show="!showDescription" class="w-full bg-gray-500 aspect-square" :src="imageUrl" :alt="imageAlt">
-    <div v-show="showDescription" class="px-10 py-5 font-bold text-left bg-white dashed-border text-primary">
-      <p>{{ description }}</p>
+    <div class="flex flex-col gap-4 aspect-square">
+      <Transition
+        mode="out-in"
+        enter-active-class="transition duration-300 ease-out transform"
+        leave-active-class="transition duration-75 ease-in transform"
+        enter-from-class="scale-95 opacity-0"
+        leave-to-class="scale-95 opacity-0"
+      >
+        <img v-if="!showDescription" class="w-full bg-gray-500 aspect-square" :src="imageUrl" :alt="imageAlt">
+        <div v-else class="px-10 py-5 font-bold text-left bg-white aspect-square dashed-border text-primary ">
+          <p>{{ description }}</p>
+        </div>
+      </Transition>
     </div>
   </CoversBaseSlide>
 </template>
