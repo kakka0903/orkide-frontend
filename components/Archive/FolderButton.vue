8d317ef7831f696955c95ea43104242d5e041993
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2023-10-16 23:12:06 +0200

    refactor: use prop over slot

--- components/Archive/FolderButton.vue
+++ components/Archive/FolderButton.vue
@@ -3,7 +3,14 @@
     <div class="border-b-2 border-dashed w-7 border-primary" />
     <FolderIcon class="w-16 h-16 transition text-primary group-hover:text-secondary-dark group-active:text-secondary group-visited:text-primary-dark" />
     <p class="text-lg font-bold underline transition text-primary group-hover:text-secondary-dark group-active:text-secondary group-visited:text-primary-dark">
-      <slot />
+      {{ title }}
     </p>
   </button>
 </template>
+
+<script setup lang="ts">
+interface Props {
+  title: string
+}
+defineProps<Props>()
+</script>
