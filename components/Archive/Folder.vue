f155c95ac199bf79e904354b7a584b579afeecb1
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2023-10-17 11:27:44 +0200

    fix: visual bug with long project titles

--- components/Archive/Folder.vue
+++ components/Archive/Folder.vue
@@ -3,9 +3,9 @@
     <button
       class="flex items-center gap-5 group text-primary"
     >
-      <div class="border-b-2 border-dashed w-7 border-primary" />
-      <FolderIcon class="w-16 h-16 transition group-hover:text-secondary-dark group-active:text-secondary group-visited:text-primary-dark" />
-      <p class="text-lg font-bold underline transition group-hover:text-secondary-dark group-active:text-secondary group-visited:text-primary-dark">
+      <div class="border-b-2 border-dashed w-7 border-primary min-w-[28px]" />
+      <FolderIcon class="w-16 h-16 min-w-[64px] transition group-hover:text-secondary-dark group-active:text-secondary group-visited:text-primary-dark" />
+      <p class="flex-grow text-lg font-bold text-left underline transition text-ellipsis group-hover:text-secondary-dark group-active:text-secondary group-visited:text-primary-dark">
         {{ title }}
       </p>
     </button>
