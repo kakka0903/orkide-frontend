d7ee3241bb1e220b1755042db3fe2086027af151
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2023-10-19 01:39:08 +0200

    fix: reactivity

--- composables/usePageData.ts
+++ composables/usePageData.ts
@@ -1,23 +1,20 @@
 import { Page } from '~/types/Pages'
 
 export const usePageData = async (routeName: string) => {
-  const { findOne } = useStrapi<Page>()
-  const strapiReq = () => findOne('pages', {
+  const { find } = useStrapi()
+  const strapiReq = () => find<Page>('pages', {
     populate: ['page_heading', 'page_intro'],
     filters: {
       path: routeName
     }
   })
+
   const { data } = await useAsyncData(
-    routeName + 'page_intro', strapiReq, {
-      transform: (response) => {
-        return response.data[0].attributes as Page | undefined
-      }
-    }
+    routeName + 'page_intro', strapiReq
   )
 
-  const heading = data.value?.page_heading
-  const intro = data.value?.page_intro
+  const heading = computed(() => data.value?.data[0].attributes.page_heading)
+  const intro = computed(() => data.value?.data[0].attributes.page_intro)
 
   return { heading, intro }
 }
