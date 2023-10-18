5b7356f3c6a763b1921e48a5c2200de02f511d3a
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2023-10-19 01:38:52 +0200

    fix: layout shift

--- components/DefaultPage.vue
+++ components/DefaultPage.vue
@@ -1,6 +1,8 @@
 <template>
   <main class="flex-grow mb-12 space-y-10">
-    <PageHeading v-if="heading !== undefined" :heading="heading" class="mt-2 mb-10" />
+    <div class="h-20 mt-2 mb-10">
+      <PageHeading v-if="heading !== undefined" :heading="heading" />
+    </div>
     <div v-if="intro !== null && intro !== undefined" class="mb-20">
       <PageIntroBox class="max-w-md" :intro="intro" />
     </div>
