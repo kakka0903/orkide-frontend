c7a79c6f9dfb6fdfd27deb2b15c6580758792836
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2023-10-23 23:43:06 +0200

    add: cache function for albumcover projects

---
+++ api/coverProjectRequests.ts
@@ -0,0 +1,28 @@
+import Strapi from 'strapi-sdk-js'
+import { AlbumCoverProject } from '~/types/CoverProjects'
+
+/**
+ * Get AlbumCoverProject API request to functions.
+ * @param strapiUrl URL to strapi instance.
+ * @returns AlbumCoverProject API request functions.
+ */
+const useCoverProjectRoutes = (strapiUrl: string) => {
+  const ROUTE_NAME = 'albumcover-projects'
+  const strapi = new Strapi({
+    url: strapiUrl
+  })
+
+  const requestOptions = {
+    populate: {
+      slides: {
+        populate: 'image'
+      }
+    }
+  }
+
+  const manyCoverProjects = () => strapi.find<AlbumCoverProject>(ROUTE_NAME, requestOptions)
+  const singleCoverProject = (coverId: number) => strapi.findOne<AlbumCoverProject>(ROUTE_NAME, coverId, requestOptions)
+  return { manyCoverProjects, singleCoverProject }
+}
+
+export { useCoverProjectRoutes }
