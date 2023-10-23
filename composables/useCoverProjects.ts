db886715720bc378cda09cfb2bc9864b4e7492bf
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2023-10-23 23:55:07 +0200

    fix: slideshow loading inconsistencies

--- composables/useCoverProjects.ts
+++ composables/useCoverProjects.ts
@@ -1,39 +1,13 @@
-import { getOnlyAttributesMany, getOnlyAttributes } from '~/api/strapiUtils'
 import { AlbumCoverProject } from '~/types/CoverProjects'
+import albumCoverProjectData from '~/data/albumCoverProjects.json'
 
-const STRAPI_COLLECTION_NAME = 'albumcover-projects'
-const requestOptions = {
-  populate: {
-    slides: {
-      populate: 'image'
-    }
-  }
+export const useManyCoverProjects = () => {
+  const data = albumCoverProjectData as AlbumCoverProject[]
+  return { data }
 }
 
-export const useManyCoverProjects = async () => {
-  const { find } = useStrapi<AlbumCoverProject>()
-
-  const strapiReq = () => find(STRAPI_COLLECTION_NAME, requestOptions)
-  const { data, error } = await useAsyncData(
-    'albumcover-projects', strapiReq, {
-      transform: (response) => {
-        return getOnlyAttributesMany(response)
-      }
-    }
-  )
-
-  return { data, error }
-}
-
-export const useSingleCoverProject = async (id: number) => {
-  const { findOne } = useStrapi<AlbumCoverProject>()
-  const strapiReq = () => findOne(STRAPI_COLLECTION_NAME, id, requestOptions)
-
-  const { data, error } = await useAsyncData(
-    'albumcover-projects-single', strapiReq, {
-      transform: getOnlyAttributes
-    }
-  )
-
-  return { data, error }
+export const useSingleCoverProject = (id: number) => {
+  const single = albumCoverProjectData.find(project => project.id === id)
+  const data = single as AlbumCoverProject
+  return { data }
 }
