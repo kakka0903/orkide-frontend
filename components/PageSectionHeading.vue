1bf92f28af5bab13ad050485d640d5ddc09404a6
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2023-12-05 00:07:26 +0100

    add: page section heading component

---
+++ components/PageSectionHeading.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex justify-center py-10 border-2 w-44 border-primary" style="border-radius: 50%;">
+    <h2 class="font-bold rotate-180 text-9xl text-primary" style="writing-mode: vertical-lr;">
+      <slot />
+    </h2>
+  </div>
+</template>
