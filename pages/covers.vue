19edb55117d75c36cecf3b4ce827d27f641cbe67
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2023-12-08 16:14:22 +0100

    refactor: extract archive display component

--- pages/covers.vue
+++ pages/covers.vue
@@ -1,56 +1,5 @@
 <template>
   <DefaultPage route="covers">
-    <div class="flex flex-row items-stretch">
-      <div class="border-r-2 border-dashed border-primary" />
-      <div class="space-y-5">
-        <ArchiveFolder
-          v-for="project in coverProjects"
-          :key="project.slug"
-          :title="project.title"
-          :is-selected="isProjectSelected(project.id)"
-          @click="selectProject(project)"
-        >
-          <div class="space-y-2">
-            <ArchiveTextLink :url="getSlideShowLink(project)">
-              <PhotoIcon class="w-6 h-6" /> Se prosessen
-            </ArchiveTextLink>
-            <ArchiveTextLink v-if="project.albumLink" :url="project.albumLink">
-              <MusicalNoteIcon class="w-6 h-6" /> Hør plata
-            </ArchiveTextLink>
-          </div>
-        </ArchiveFolder>
-      </div>
-      <NuxtPage />
-    </div>
+    <ArchiveDisplay />
   </DefaultPage>
 </template>
-
-<script setup lang="ts">
-import { PhotoIcon, MusicalNoteIcon } from '@heroicons/vue/24/outline'
-import { AlbumCoverProject } from '~/types/CoverProjects'
-
-const { data: coverProjects } = await useManyCoverProjects()
-
-function getSlideShowLink (coverProject: AlbumCoverProject) {
-  return '/covers/' + coverProject.id
-}
-
-const selectedProjectID: Ref<null | number> = ref(null)
-
-/**
- * Check if project is selected or not.
- * @param projectId ID of project to check.
- */
-function isProjectSelected (projectId: number) {
-  return selectedProjectID.value === projectId
-}
-
-/**
- * Select a project when its not selected and unselect it when it is selected.
- * @param project The project to select/unselect.
- */
-function selectProject (project: AlbumCoverProject) {
-  const isSelected = isProjectSelected(project.id)
-  selectedProjectID.value = (isSelected) ? null : project.id
-}
-</script>
