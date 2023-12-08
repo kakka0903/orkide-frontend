87908f91eca51efae63ddb57490c2a7efb451ee1
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2023-12-08 16:19:10 +0100

    refactor: extract video project display component

---
+++ components/VideoProjectsDisplay.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="flex flex-col content-start flex-grow gap-2 mb-2 gap-y-3 sm:flex-wrap sm:flex-row">
+      <ProjectTicket
+        v-for="musicVideo in projects.data"
+        :key="musicVideo.id"
+        :music-video="musicVideo"
+      />
+    </div>
+    <NuxtPage :projects="projects.data" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import projects from '~/data/projects.json'
+</script>
--- pages/prosjekter.vue
+++ pages/prosjekter.vue
@@ -1,20 +1,12 @@
 <template>
   <DefaultPage route="prosjekter">
-    <NuxtPage :projects="projects.data" />
-    <div class="flex flex-col content-start flex-grow gap-2 mb-2 gap-y-3 sm:flex-wrap sm:flex-row">
-      <ProjectTicket
-        v-for="musicVideo in projects.data"
-        :key="musicVideo.id"
-        :music-video="musicVideo"
-      />
-    </div>
+    <VideoProjectsDisplay />
   </DefaultPage>
 </template>
 
 <script setup>
 // TODO: disable zoom on double tap
 
-import projects from '~/data/projects.json'
 useHead({
   title: 'Orkidé - Prosjekter',
   meta: [{ name: 'description', content: 'Se de nye Undergrunn videoene på Orkidé Kino. Alle videoer er produsert, filmet og redigert av Orkidé. Sjekk ut hits som ITALIA, DR.PLAZA og TRAPMOBILE.' }],
