67c379479e2711659516432f60220be9ed29911f
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2023-12-08 17:45:36 +0100

    add: section switcher component

---
+++ components/SectionSwitcher.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="flex">
+    <div class="flex-grow">
+      <Transition
+        enter-active-class="transition transform"
+        enter-from-class="opacity-0"
+        mode="out-in"
+      >
+        <component :is="currentSection.component" :key="currentSection.title" />
+      </Transition>
+    </div>
+    <div class="flex flex-col items-center gap-5">
+      <PageSectionHeading>
+        <Transition
+          enter-active-class="transition transform"
+          enter-from-class="opacity-0"
+          mode="out-in"
+        >
+          <p :key="currentSection.title">
+            {{ currentSection.title }}
+          </p>
+        </Transition>
+      </PageSectionHeading>
+      <SwitchSectionButton @click="nextSection()">
+        {{ switchMessage }}
+      </SwitchSectionButton>
+      <p class="text-primary">
+        {{ sectionIdx + 1 }} / {{ sections.length }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  sections: {
+    title: string,
+    component: any
+  }[],
+  switchMessage: string
+}
+const props = defineProps<Props>()
+
+const sectionIdx = ref(0)
+const currentSection = computed(() => {
+  return props.sections[sectionIdx.value]
+})
+
+const updateSectionUrl = () => {
+  console.log('set url section to: ' + currentSection.value.title)
+}
+
+const nextSection = () => {
+  sectionIdx.value = (sectionIdx.value + 1) % props.sections.length
+  updateSectionUrl()
+}
+
+onMounted(updateSectionUrl)
+</script>
