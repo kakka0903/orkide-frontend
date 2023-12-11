7757bbd0a479bc7b64aa1f85bb8bb0f41de5e803
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2023-12-11 16:30:18 +0100

    fix: text wrapping issue in page heading

--- components/PageHeading.vue
+++ components/PageHeading.vue
@@ -10,7 +10,7 @@
         <svg v-show="appearShow" class="stroke-current text-secondary w-72" viewBox="0 0 735.255 133.583"><path data-name="Path 62" d="M735.193 66.52 81.458 79.33l-14.481 53.468L56.395 79.33.695 66.52l55.7-13.366L66.977.798l14.481 52.356Z" fill="none" stroke-width="6" /></svg>
       </transition>
     </div>
-    <h1 class="absolute top-[-5px] left-[18px] text-[43px]">
+    <h1 class="absolute top-[-5px] left-[18px] text-[43px] whitespace-nowrap">
       {{ heading.title }}
     </h1>
     <transition
