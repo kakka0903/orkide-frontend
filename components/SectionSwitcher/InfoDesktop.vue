cd9e1da3ee1f2b759be3ad4b7f050643547cbc0b
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2023-12-11 16:45:39 +0100

    refactor: embed design component

--- components/SectionSwitcher/InfoDesktop.vue
+++ components/SectionSwitcher/InfoDesktop.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="flex flex-col items-center gap-5">
-    <SectionSwitcherHeading>
-      <Transition
-        enter-active-class="transition transform"
-        enter-from-class="opacity-0"
-        mode="out-in"
-      >
-        <p :key="currentSectionTitle">
-          {{ currentSectionTitle }}
-        </p>
-      </Transition>
-    </SectionSwitcherHeading>
+    <div class="flex justify-center py-10 border-2 w-44 border-primary" style="border-radius: 50%;">
+      <h2 class="font-bold rotate-180 text-9xl text-primary" style="writing-mode: vertical-lr;">
+        <Transition
+          enter-active-class="transition transform"
+          enter-from-class="opacity-0"
+          mode="out-in"
+        >
+          <p :key="currentSectionTitle">
+            {{ currentSectionTitle }}
+          </p>
+        </Transition>
+      </h2>
+    </div>
+
     <SectionSwitcherButton @click="$emit('switchSection')">
       {{ switchMessage }}
     </SectionSwitcherButton>
+
     <p class="text-primary">
       {{ sectionIdx + 1 }} / {{ numSections }}
     </p>
