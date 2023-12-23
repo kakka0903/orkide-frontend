2ea543fc772332472db5285c13fe809e2de74ebf
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2023-12-23 17:48:27 +0100

    fix: slide occupies all available space

--- components/Covers/BaseSlide.vue
+++ components/Covers/BaseSlide.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full max-w-lg px-5 py-20 font-mono md:px-10 md:max-w-lg" @click.stop>
-    <div class="shadow-xl">
+    <div class="w-full shadow-xl">
       <slot />
     </div>
   </div>
