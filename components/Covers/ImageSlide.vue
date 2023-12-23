735fe444b367a9e9300533d6fbf2dd992f8248ce
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2023-12-23 17:26:03 +0100

    fix: allow non-square images

--- components/Covers/ImageSlide.vue
+++ components/Covers/ImageSlide.vue
@@ -1,6 +1,6 @@
 <template>
   <CoversBaseSlide>
-    <div class="flex flex-col w-full gap-4 cursor-pointer aspect-square" @click="$emit('switch')">
+    <div class="flex flex-col justify-center w-full gap-4 cursor-pointer aspect-square" @click="$emit('switch')">
       <Transition
         mode="out-in"
         enter-active-class="transition duration-300 ease-out transform-gpu"
@@ -8,7 +8,7 @@
         enter-from-class="scale-95 opacity-0"
         leave-to-class="scale-95 opacity-0"
       >
-        <img v-if="!showDescription" class="bg-gray-500 aspect-square" :src="imageUrl" :alt="imageAlt">
+        <img v-if="!showDescription" class="bg-gray-500" :src="imageUrl" :alt="imageAlt">
         <DashedBox v-else class="px-10 py-5 text-left bg-background aspect-square text-primary ">
           <MarkdownField :markdown="description" />
         </DashedBox>
