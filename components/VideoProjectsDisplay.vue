067eb3ce13cf17adb96e7c463ca8d1bd463be86b
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-02-03 15:17:26 +0100

    refactor: load video project from cms

--- components/VideoProjectsDisplay.vue
+++ components/VideoProjectsDisplay.vue
@@ -1,16 +1,34 @@
 <template>
   <div>
-    <div class="flex flex-col content-start flex-grow gap-2 mb-2 gap-y-3 sm:flex-wrap sm:flex-row">
+    <div
+      v-if="projects !== null && projects !== undefined"
+      class="flex flex-col content-start flex-grow gap-2 mb-2 gap-y-3 sm:flex-wrap sm:flex-row"
+    >
       <ProjectTicket
-        v-for="musicVideo in projects.data"
+        v-for="musicVideo in projects"
         :key="musicVideo.id"
         :music-video="musicVideo"
       />
+      <NuxtPage :projects="projects.data" />
     </div>
-    <NuxtPage :projects="projects.data" />
   </div>
 </template>
 
 <script setup lang="ts">
-import projects from '~/data/projects.json'
+
+const strapi = useStrapi()
+const doCacheCMSData = useRuntimeConfig().public.cacheCMSData
+
+const getVideoProjects = () => strapi.find('projects', {
+  sort: ['release_date:desc']
+})
+
+const asyncOptions = {
+  transform: res => res.data.map(project => project.attributes),
+  server: doCacheCMSData
+}
+
+const { data: projects } = await useAsyncData(
+  getVideoProjects, asyncOptions
+)
 </script>
