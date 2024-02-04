af6dc84af500e9a2ac095a7e8a0e1714b277f8b3
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-02-04 21:20:57 +0100

    fix: missing null check

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -1,29 +1,31 @@
 <template>
   <DefaultPage route="kontakt">
-    <div class="flex flex-col items-center mt-12 mb-24 font-mono font-bold text-secondary text-7xl sm:text-8xl">
-      <a
-        class="bg-background text-primary hover:bg-primary active:text-secondary hover:text-background"
-        :href="contact.instagram_link"
-      >{{ contact.instagram_link_text }}</a>
-      <a
-        class="bg-background text-secondary hover:bg-secondary active:text-primary hover:text-background"
-        :href="contact.youtube_link"
-      >{{ contact.youtube_link_text }}</a>
-      <a
-        class="bg-background text-primary hover:bg-primary active:text-secondary hover:text-background"
-        :href="contact.tiktok_link"
-      >{{ contact.tiktok_link_text }} </a>
+    <div v-if="contact !== null">
+      <div class="flex flex-col items-center mt-12 mb-24 font-mono font-bold text-secondary text-7xl sm:text-8xl">
+        <a
+          class="bg-background text-primary hover:bg-primary active:text-secondary hover:text-background"
+          :href="contact.instagram_link"
+        >{{ contact.instagram_link_text }}</a>
+        <a
+          class="bg-background text-secondary hover:bg-secondary active:text-primary hover:text-background"
+          :href="contact.youtube_link"
+        >{{ contact.youtube_link_text }}</a>
+        <a
+          class="bg-background text-primary hover:bg-primary active:text-secondary hover:text-background"
+          :href="contact.tiktok_link"
+        >{{ contact.tiktok_link_text }} </a>
+      </div>
+      <p class="font-bold text-primary">
+        For business ta kontakt på <a
+          class="underline text-secondary"
+          :href="'mailto:' + contact.contact_email"
+        >{{ contact.contact_email }}</a>
+      </p>
     </div>
-    <p class="font-bold text-primary">
-      For business ta kontakt på <a
-        class="underline text-secondary"
-        :href="'mailto:' + contact.contact_email"
-      >{{ contact.contact_email }}</a>
-    </p>
   </DefaultPage>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const cacheCMSData = useRuntimeConfig().public.cacheCMSData
 const { getContactData } = useCMSData(cacheCMSData)
 const { data: contact } = await getContactData()
