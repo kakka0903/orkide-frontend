acbb33cbea624809c5c07ef48e84ee30fd7b5981
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-02-10 15:01:52 +0100

    add: types slide definitions

---
+++ types/TicketSlideshow.ts
@@ -0,0 +1,19 @@
+export interface TicketsIntroSlide {
+  title: string
+  subtitle: string
+  description: string
+}
+
+export interface TicketsImageSlide {
+  image: object
+}
+
+export interface TicketsVideoSlide {
+  youtube_id: string
+}
+
+export interface TicketsSlideshow {
+  intro_slide: TicketsIntroSlide
+  image_slides: TicketsImageSlide[]
+  video_slides: TicketsVideoSlide[]
+}
