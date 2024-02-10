3616aaaa155261ec5952631e57c258b40678efb9
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-02-10 14:37:58 +0100

    fix: naming conflict

--- pages/tickets-slideshow.vue
+++ pages/tickets-slideshow.vue
@@ -3,11 +3,10 @@
     <BoxButton @click="showSlideshow = !showSlideshow">
       open slideshow
     </BoxButton>
-    <TicketsSlideshow v-if="showSlideshow" :cover-project-id="1" />
+    <TicketsShow v-if="showSlideshow" :cover-project-id="1" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { XMarkIcon } from '@heroicons/vue/24/outline'
 const showSlideshow = ref(false)
 </script>
