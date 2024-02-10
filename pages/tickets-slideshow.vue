6911583b6f43ce30ab22cfb7cf4fe2c0a094fb6b
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-02-10 14:27:44 +0100

    add: testing page

---
+++ pages/tickets-slideshow.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex-grow">
+    <BoxButton @click="showSlideshow = !showSlideshow">
+      open slideshow
+    </BoxButton>
+    <TicketsSlideshow v-if="showSlideshow" :cover-project-id="1" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { XMarkIcon } from '@heroicons/vue/24/outline'
+const showSlideshow = ref(false)
+</script>
