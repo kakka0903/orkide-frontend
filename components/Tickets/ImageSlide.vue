fa9496134ef08ac6874871a934bfc023f8b3939a
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-02-12 09:59:03 +0100

    refactor: simpify slide centering styles

--- components/Tickets/ImageSlide.vue
+++ components/Tickets/ImageSlide.vue
@@ -1,14 +1,12 @@
 <template>
   <TicketsBaseSlide>
-    <div class="flex flex-col items-center justify-center w-full gap-4 cursor-grab aspect-square">
-      <NuxtImg
-        class="bg-gray-500"
-        provider="strapi"
-        :modifiers="{ breakpoint: 'large' }"
-        :src="imageUrl"
-        :alt="imageAlt"
-      />
-    </div>
+    <NuxtImg
+      class="bg-gray-500"
+      provider="strapi"
+      :modifiers="{ breakpoint: 'large' }"
+      :src="imageUrl"
+      :alt="imageAlt"
+    />
   </TicketsBaseSlide>
 </template>
 
