cc63e952708f65658c3667d658bedf06d3d7d3e8
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-02-15 08:58:55 +0100

    refactor: same uri for all slideshow links

---
+++ pages/prosjekter/[slug].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <TicketsShow :slideshow="slideshow" @close="goBack" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const doCacheCMSData = useRuntimeConfig().public.cacheCMSData
+const { getSlideshowBySlug } = useCMSData(doCacheCMSData)
+const { data: slideshow } = await getSlideshowBySlug(route.params.slug)
+const goBack = () => navigateTo('/prosjekter?section=kino')
+// TODO: add back SEO meta
+</script>
